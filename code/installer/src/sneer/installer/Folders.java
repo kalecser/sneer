a751e6c4eaadace013209d88ade63668194521c9
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2009-08-13 06:01:29 -0300

    Installer fixed.

--- code/installer/src/sneer/installer/Folders.java
+++ code/installer/src/sneer/installer/Folders.java
@@ -9,7 +9,6 @@ public class Folders extends SneerDirectories {
 	static File SNEER_HOME() { return SNEER_HOME; }
 	static File DATA() { return DATA; }
 	static File LOG_FILE() { return LOG_FILE; }
-	static File CODE() { return CODE; }
 	static File OWN_CODE() { return OWN_CODE; }
 	static File OWN_BIN() { return OWN_BIN; }
 	static File PLATFORM_CODE() { return PLATFORM_CODE; }
--- code/platform/src/sneer/main/SneerDirectories.java
+++ code/platform/src/sneer/main/SneerDirectories.java
@@ -8,7 +8,9 @@ public class SneerDirectories {
 	protected static final File DATA 			= new File(SNEER_HOME, dummyPrefix() + "data");
 	protected static final File TMP 			= new File(SNEER_HOME, dummyPrefix() + "tmp");
 	protected static final File LOG_FILE 		= new File(SNEER_HOME, dummyPrefix() + "logs/log.txt");
+	protected static final File OWN_CODE 		= new File(SNEER_HOME, "code/own");
 	protected static final File OWN_BIN 		= new File(SNEER_HOME, "code/own/bin");
+	protected static final File PLATFORM_CODE 	= new File(SNEER_HOME, "code/platform");
 	protected static final File PLATFORM_SRC 	= new File(SNEER_HOME, "code/platform/src");
 	protected static final File PLATFORM_BIN 	= new File(SNEER_HOME, "code/platform/bin");
 
