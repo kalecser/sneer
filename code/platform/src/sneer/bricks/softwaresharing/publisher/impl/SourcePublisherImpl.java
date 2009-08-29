8f9c5605720e1a274e8f3160c8a688bda6d69364
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2009-08-29 05:10:03 -0300

    Standardizing src file lastModified dates because git does not preserve them.

--- code/platform/src/sneer/bricks/softwaresharing/publisher/impl/SourcePublisherImpl.java
+++ code/platform/src/sneer/bricks/softwaresharing/publisher/impl/SourcePublisherImpl.java
@@ -4,7 +4,11 @@ import static sneer.foundation.environments.Environments.my;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
+import sneer.bricks.hardware.io.IO;
 import sneer.bricks.hardwaresharing.files.publisher.FilePublisher;
 import sneer.bricks.pulp.blinkinglights.BlinkingLights;
 import sneer.bricks.pulp.blinkinglights.Light;
@@ -17,11 +21,14 @@ import sneer.bricks.softwaresharing.publisher.SrcFolderHash;
 
 class SourcePublisherImpl implements SourcePublisher {
 	
+	private static final long AN_ARBITRARY_DATE = 1093489200000L; //26/08/2004 (UTC -3), the publication date of the sovereign computing manifesto. 
 	private final Light _errorLight = my(BlinkingLights.class).prepare(LightType.ERROR);
 
 	
 	@Override
 	public void publishSourceFolder() {
+		standardizeLastModifiedDatesWhileWeStillUseGitBecauseGitDoesNotPreserveThem();
+		
 		Sneer1024 hash;
 		try {
 			hash = my(FilePublisher.class).publish(platformSrcFolder());
@@ -31,6 +38,31 @@ class SourcePublisherImpl implements SourcePublisher {
 		}
 		
 		my(TupleSpace.class).publish(new SrcFolderHash(hash));
+	}
+
+
+	private void standardizeLastModifiedDatesWhileWeStillUseGitBecauseGitDoesNotPreserveThem() {
+		Set<File> foldersVisited = new HashSet<File>();
+		
+		Iterator<File> srcFiles = srcFiles();
+		while (srcFiles.hasNext()) {
+			File file = srcFiles.next();
+			standardizeLastModified(file);
+
+			File folder = file.getParentFile();
+			if (foldersVisited.add(folder))
+				standardizeLastModified(folder);
+		}
+	}
+
+
+	private boolean standardizeLastModified(File fileOrFolder) {
+		return fileOrFolder.setLastModified(AN_ARBITRARY_DATE);
+	}
+
+
+	private Iterator<File> srcFiles() {
+		return my(IO.class).files().iterate(platformSrcFolder(), null, true);
 	}
 
 
