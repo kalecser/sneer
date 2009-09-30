a95bde80328ebe480ed006fd6e0237884cf1591f
igorarouca <igor.arouca@gmail.com>
2009-09-29 22:06:00 -0300

    TrackSourceStrategy refactoring (work in progress)

--- code/platform/src/dfcsantos/wusic/impl/PeerTracks.java
+++ code/platform/src/dfcsantos/wusic/impl/PeerTracks.java
@@ -1,21 +1,14 @@
 package dfcsantos.wusic.impl;
 
-import dfcsantos.wusic.Track;
-
 class PeerTracks extends TrackSourceStrategy {
 
 	static final TrackSourceStrategy INSTANCE = new PeerTracks();
 
 	private PeerTracks() {};
-	
-	@Override
-	Track nextTrack() {
-		throw new sneer.foundation.lang.exceptions.NotImplementedYet(); // Implement
-	}
 
 	@Override
-	void noWay(Track rejected) {
-		throw new sneer.foundation.lang.exceptions.NotImplementedYet(); // Implement
+	String tracksSubfolder() {
+		return "candidates";
 	}
 
 	void meToo() {
