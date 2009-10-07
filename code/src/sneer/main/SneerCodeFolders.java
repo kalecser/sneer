b7d2a88e9d883abe096ebf00dab37a705af94869
igorarouca <igor.arouca@gmail.com>
2009-10-07 11:45:52 -0300

    Structure refactoring: 	- 'platform' removed 	- 'platform/own' renamed to 'code-own'

---
+++ code/src/sneer/main/SneerCodeFolders.java
@@ -0,0 +1,23 @@
+package sneer.main;
+
+import java.io.File;
+
+public class SneerCodeFolders {
+
+	public static final File SNEER_HOME = sneerHome();
+
+	public static final File CODE = new File(SNEER_HOME, "code");
+
+	public static final File SRC = new File(CODE, "src");
+	public static final File BIN = new File(CODE, "bin");
+	public static final File STAGE = new File(CODE, "stage");
+
+	private static File sneerHome() {
+		String override = System.getProperty("sneer.home");
+		if (override != null)
+			return new File(override);
+
+		return new File(System.getProperty("user.home"), "sneer");
+	}
+
+}
