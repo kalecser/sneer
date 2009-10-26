0491b9e0a78246402d83a8229c66b062041a7da1
igorarouca <igor.arouca@gmail.com>
2009-10-26 21:37:07 -0200

    TracksMonitor (work in progress)

--- code/src/dfcsantos/tracks/monitor/impl/DownloadedTracksMonitorImpl.java
+++ code/src/dfcsantos/tracks/monitor/impl/DownloadedTracksMonitorImpl.java
@@ -1,13 +1,24 @@
 package dfcsantos.tracks.monitor.impl;
 
+import static sneer.foundation.environments.Environments.my;
+
+import java.io.File;
+
 import dfcsantos.tracks.Track;
+import dfcsantos.tracks.Tracks;
+import dfcsantos.tracks.folder.TracksFolderKeeper;
 import dfcsantos.tracks.monitor.DownloadedTracksMonitor;
 
 class DownloadedTracksMonitorImpl implements DownloadedTracksMonitor {
 
 	@Override
 	public boolean isTrackAlreadyDownloaded(Track trackToCheck) {
-		throw new sneer.foundation.lang.exceptions.NotImplementedYet(); // Implement
+		return isTrackInTheFolder(trackToCheck, my(TracksFolderKeeper.class).sharedTracksFolder().currentValue())
+			|| isTrackInTheFolder(trackToCheck, my(TracksFolderKeeper.class).peerTracksFolder());
+	}
+
+	private boolean isTrackInTheFolder(Track track, File folder) {
+		return my(Tracks.class).listMp3FilesFromFolder(folder).contains(track.file());
 	}
 
 }
