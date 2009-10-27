31543739e24f0e5c8681d6eea1265fa5ee3aec2d
igorarouca <igor.arouca@gmail.com>
2009-10-27 20:02:13 -0200

    Playlist tests unignored

--- code/src/dfcsantos/tracks/playlist/impl/AbstractPlaylist.java
+++ code/src/dfcsantos/tracks/playlist/impl/AbstractPlaylist.java
@@ -3,8 +3,8 @@ package dfcsantos.tracks.playlist.impl;
 import static sneer.foundation.environments.Environments.my;
 
 import java.io.File;
-import java.util.Enumeration;
 import java.util.List;
+import java.util.ListIterator;
 
 import sneer.bricks.pulp.blinkinglights.BlinkingLights;
 import sneer.bricks.pulp.blinkinglights.Light;
@@ -13,50 +13,50 @@ import dfcsantos.tracks.Track;
 import dfcsantos.tracks.Tracks;
 import dfcsantos.tracks.playlist.Playlist;
 
-abstract class AbstractPlaylist implements Playlist, Enumeration<Track> {
+abstract class AbstractPlaylist implements Playlist {
 
 	private final File _tracksFolder;
-	private List<File> _trackFiles;
+	private ListIterator<File> _trackIterator;
 
 	private final Light _noTracksFound = my(BlinkingLights.class).prepare(LightType.WARN);
 
 	AbstractPlaylist(File tracksFolder) {
 		_tracksFolder = tracksFolder;
-		loadTracks();
+		initTrackIterator();
 	}
 
-	List<File> trackFiles() {
-		return _trackFiles;
+	private void initTrackIterator() {
+		_trackIterator = trackFiles().listIterator();	
 	}
 
-	List<File> searchTracks(File folder) {
-		return my(Tracks.class).listMp3FilesFromFolder(folder);
+	private List<File> trackFiles() {
+		List<File> tracks = my(Tracks.class).listMp3FilesFromFolder(_tracksFolder);
+		setTracksOrder(tracks);
+
+		return tracks;
 	}
 
-	private void loadTracks() {
-		_trackFiles = searchTracks(_tracksFolder);		
-	}
+	abstract void setTracksOrder(List<File> tracks);
 
 	@Override
 	public Track nextTrack() {
-		if (!hasMoreElements()) {
+		if (!_trackIterator.hasNext()) {
 			rescan();
-			if (!hasMoreElements()) {
+			if (!_trackIterator.hasNext()) {
 				my(BlinkingLights.class).turnOnIfNecessary(_noTracksFound, "No Tracks Found", "Please choose a folder with MP3 files in it or in its subfolders (Wusic > File > Configure Root Track Folder).");
 				return null;
 			}
 		}
 		my(BlinkingLights.class).turnOffIfNecessary(_noTracksFound);
-		return nextElement();
+
+		final Track nextTrack = my(Tracks.class).newTrack(_trackIterator.next());
+		_trackIterator.remove();
+
+		return nextTrack;
 	}
 
-	@Override
-	public boolean hasMoreElements() {
-		return !trackFiles().isEmpty();
-	}
-
-	void rescan() {
-		loadTracks();
+	private void rescan() {
+		initTrackIterator();
 	}
 
 }
--- code/src/dfcsantos/tracks/playlist/impl/SequentialPlaylist.java
+++ code/src/dfcsantos/tracks/playlist/impl/SequentialPlaylist.java
@@ -1,41 +1,21 @@
 package dfcsantos.tracks.playlist.impl;
 
-import static sneer.foundation.environments.Environments.my;
-
 import java.io.File;
-import java.util.ListIterator;
-
-import dfcsantos.tracks.Track;
-import dfcsantos.tracks.Tracks;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 class SequentialPlaylist extends AbstractPlaylist {
 
-	private ListIterator<File> _tracksIterator;
-
 	SequentialPlaylist(File tracksFolder) {
 		super(tracksFolder);
-		initIterator();
-	}
-
-	private void initIterator() {
-		_tracksIterator = trackFiles().listIterator();	
 	}
 
 	@Override
-	public boolean hasMoreElements() {
-		return _tracksIterator.hasNext();
-	}
-
-	@Override
-	public Track nextElement() {
-		// Note that the returned element is NOT removed from the list like in RandomPlaylist
-		return my(Tracks.class).newTrack(_tracksIterator.next());
-	}
-
-	@Override
-	void rescan() {
-		// One new iterator is used for each iteration sequence 
-		initIterator();
+	void setTracksOrder(List<File> tracks) {
+		Collections.sort(tracks, new Comparator<File>() { @Override public int compare(File file1, File file2) {
+			return file1.compareTo(file2);
+		}});
 	}
 
 }
