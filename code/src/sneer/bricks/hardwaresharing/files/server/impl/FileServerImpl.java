c9bd4ce2f771b9da00f4abd897ec0cf6f5c06b3b
igorarouca <igor.arouca@gmail.com>
2009-11-03 17:52:02 -0200

    Safe clause added to avoid OutOfMemoryExceptions while sending FileContents P.S. This code must be removed after file blocks implementation.

--- code/src/sneer/bricks/hardwaresharing/files/server/impl/FileServerImpl.java
+++ code/src/sneer/bricks/hardwaresharing/files/server/impl/FileServerImpl.java
@@ -9,6 +9,7 @@ import sneer.bricks.hardware.cpu.lang.contracts.WeakContract;
 import sneer.bricks.hardware.io.IO;
 import sneer.bricks.hardware.io.log.Logger;
 import sneer.bricks.hardware.ram.arrays.ImmutableArrays;
+import sneer.bricks.hardware.ram.meter.MemoryMeter;
 import sneer.bricks.hardwaresharing.files.map.FileMap;
 import sneer.bricks.hardwaresharing.files.protocol.BigFileBlocks;
 import sneer.bricks.hardwaresharing.files.protocol.FileContents;
@@ -37,35 +38,29 @@ public class FileServerImpl implements FileServer, Consumer<FileRequest> {
 	@Override
 	public void consume(FileRequest request) {
 		try {
-			reply(request);
+			replyIfThereIsEnoughMemoryAvailable(request);
 		} catch (IOException e) {
 			my(BlinkingLights.class).turnOn(LightType.ERROR, "Error trying to reply FileServer request: " + request, "This might indicate a problem with your file device.", e, 30000);
 		}
 	}
 
 
-	private void reply(FileRequest request) throws IOException {
+	private void replyIfThereIsEnoughMemoryAvailable(FileRequest request) throws IOException {
 		Object response = getContents(request);
-		
+
 		if (response == null) {
 			my(Logger.class).log("FileCache miss.");
 			return;
 		}
-		
+
 		Tuple reply = asTuple(request.publisher(), response);
-		my(TupleSpace.class).publish(reply);
-
-		logFolderActivity(reply);
-
-	}
-
-
-	private void logFolderActivity(Tuple reply) {
-		if (reply instanceof FolderContents) {
-			my(Logger.class).log("Sending Folder Contents:");
-			for (FileOrFolder fileOrFolder : ((FolderContents)reply).contents)
-				my(Logger.class).log("   FileOrFolder: {} date: {} hash: {}", fileOrFolder.name, fileOrFolder.lastModified, fileOrFolder.hashOfContents);
+		if (reply == null) {
+			my(Logger.class).log("FileServer request not answered due to lack of memory: " + request);
+			return;
 		}
+
+		my(TupleSpace.class).publish(reply);
+		logFolderActivity(reply);
 	}
 
 
@@ -81,13 +76,31 @@ public class FileServerImpl implements FileServer, Consumer<FileRequest> {
 		if (response instanceof FolderContents)
 			return new FolderContents(((FolderContents)response).contents);
 		
-		if (response instanceof File)
-			return asFileContents(addressee, (File)response);
+		if (response instanceof File) {
+			File fileToBeSent = (File)response;
+			if (!isThereEnoughMemoryFor(fileToBeSent)) return null;
+			return asFileContents(addressee, fileToBeSent);
+		}
 		
 		if (response instanceof BigFileBlocks)
 			return new BigFileBlocks(((BigFileBlocks)response)._contents);
 			
 		throw new IllegalStateException("I don know how to obtain a tuple from type: " + response.getClass());
+	}
+
+
+	private boolean isThereEnoughMemoryFor(File response) {
+		return availableMemory() > safeMemoryLimitFor(response);
+	}
+
+
+	private int availableMemory() {
+		return my(MemoryMeter.class).maxMBs() - my(MemoryMeter.class).usedMBs().currentValue();
+	}
+
+
+	private int safeMemoryLimitFor(File response) {
+		return (int) (3 * response.length() / (1024 * 1024));
 	}
 
 
@@ -98,5 +111,12 @@ public class FileServerImpl implements FileServer, Consumer<FileRequest> {
 	}
 
 	
+	private void logFolderActivity(Tuple reply) {
+		if (reply instanceof FolderContents) {
+			my(Logger.class).log("Sending Folder Contents:");
+			for (FileOrFolder fileOrFolder : ((FolderContents)reply).contents)
+				my(Logger.class).log("   FileOrFolder: {} date: {} hash: {}", fileOrFolder.name, fileOrFolder.lastModified, fileOrFolder.hashOfContents);
+		}
+	}
 
 }
