c08496ad25a08661955ba2d72f1e0c7972ffc69d
igorarouca <igor.arouca@gmail.com>
2009-11-16 17:30:44 -0200

    FileClientTest implemented

--- code/src/sneer/bricks/hardwaresharing/files/client/test/FileClientTest.java
+++ code/src/sneer/bricks/hardwaresharing/files/client/test/FileClientTest.java
@@ -5,28 +5,30 @@ import static sneer.foundation.environments.Environments.my;
 import java.io.File;
 import java.io.IOException;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
+import sneer.bricks.hardware.io.IO;
 import sneer.bricks.hardwaresharing.files.client.FileClient;
 import sneer.bricks.hardwaresharing.files.map.FileMap;
+import sneer.bricks.hardwaresharing.files.protocol.FileRequest;
 import sneer.bricks.pulp.crypto.Sneer1024;
+import sneer.bricks.pulp.tuples.TupleSpace;
 import sneer.bricks.software.code.classutils.ClassUtils;
 import sneer.bricks.software.folderconfig.tests.BrickTest;
+import sneer.foundation.lang.Consumer;
 
 public class FileClientTest extends BrickTest {
 
-	private final FileMap _fileMap = mock(FileMap.class);
-
-	@Ignore
-	@Test
-	public void downloadFileOnlyOnce() throws IOException {
-		final Sneer1024 hash = _fileMap.put(myClassFile());
-
-		// TODO: Create expectation to check that the file has not been requested twice 
-		my(FileMap.class).put(myClassFile());
-		my(FileClient.class).fetchFile(myClassFile(), hash);
-		my(FileClient.class).fetchFile(myClassFile(), hash);
+	@Test (timeout = 2000)
+	public void fileAlreadyMappedIsNotDownloaded() throws IOException {
+		final Sneer1024 hash = my(FileMap.class).put(myClassFile());
+		final File tmpFile = createTmpFile("tmpFile");
+		my(TupleSpace.class).addSubscription(FileRequest.class, new Consumer<FileRequest>() { @Override public void consume(FileRequest request) {
+			throw new IllegalStateException();
+		}});
+		my(FileClient.class).fetchFile(tmpFile, hash);
+		my(IO.class).files().assertSameContents(tmpFile, myClassFile());
+		my(TupleSpace.class).waitForAllDispatchingToFinish();
 	}
 
 	private File myClassFile() {
