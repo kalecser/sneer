5d3ea08c08ee0e1debe52b0676d36a25fce10b32
igorarouca <igor.arouca@gmail.com>
2009-11-17 17:48:31 -0200

    FileCopyTestBase.testWithFolder() unignored

--- code/src/sneer/bricks/hardwaresharing/files/tests/FileCopyTestBase.java
+++ code/src/sneer/bricks/hardwaresharing/files/tests/FileCopyTestBase.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import sneer.bricks.hardware.io.IO;
@@ -31,13 +30,12 @@ public abstract class FileCopyTestBase extends BrickTest {
 		testWith(anySmallFile());
 	}
 
-	@Ignore
-	@Test (timeout = 6000)
+	@Test (timeout = 5000)
 	public void testWithFolder() throws IOException {
 		testWith(folderWithAFewFiles());
 	}
 
-	@Test (timeout = 6000)
+	@Test (timeout = 5000)
 	public void testWithLargeFile() throws IOException {
 		testWith(createLargeFile());
 	}
