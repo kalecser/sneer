ff2bbcf4f16cdef3b3dd495eee271b7edfd03bdf
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2009-11-26 15:48:55 -0200

    New log filter defaults.

--- code/src/sneer/bricks/hardware/io/log/filter/impl/LogFilterImpl.java
+++ code/src/sneer/bricks/hardware/io/log/filter/impl/LogFilterImpl.java
@@ -14,12 +14,13 @@ class LogFilterImpl implements LogFilter {
 	{
 		_phrases.add("Exception");
 		_phrases.add("thrown");
-//		_phrases.add("online");
-//		_phrases.add("offline");
+		_phrases.add("online");
+		_phrases.add("offline");
 //		_phrases.add("Tuple");
 		_phrases.add("Fetch");
 		_phrases.add("Track");
 		_phrases.add("File");
+		_phrases.add("Mapping");
 //		_phrases.add("Shout");
 //		_phrases.add("Stepper");
 //		_phrases.add("Sending");
