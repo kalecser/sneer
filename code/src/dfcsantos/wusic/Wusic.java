d55faff955b2309fb1587a07685d4c68025b50a9
igorarouca <igor.arouca@gmail.com>
2010-01-05 17:06:16 -0200

    #Wusic and #Wind are no longer snapps (but WusicGui and WindGui are)

--- code/src/dfcsantos/wusic/Wusic.java
+++ code/src/dfcsantos/wusic/Wusic.java
@@ -3,11 +3,9 @@ package dfcsantos.wusic;
 import java.io.File;
 
 import sneer.bricks.pulp.reactive.Signal;
-import sneer.bricks.software.bricks.snappstarter.Snapp;
 import sneer.foundation.brickness.Brick;
 import sneer.foundation.lang.PickyConsumer;
 
-@Snapp
 @Brick
 public interface Wusic {
 
