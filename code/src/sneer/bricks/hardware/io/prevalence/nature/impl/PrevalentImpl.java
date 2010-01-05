9df6abb4349d0f67f0a6df0b71fc6cdf86ca2105
Klaus <klauswuestefeld@gmail.com>
2010-01-05 02:41:01 -0200

    PrevalentImpl closes prevaylers on crash.

--- code/src/sneer/bricks/hardware/io/prevalence/nature/impl/PrevalentImpl.java
+++ code/src/sneer/bricks/hardware/io/prevalence/nature/impl/PrevalentImpl.java
@@ -4,18 +4,34 @@ import static sneer.foundation.environments.Environments.my;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.prevayler.Prevayler;
 import org.prevayler.PrevaylerFactory;
 
+import sneer.bricks.hardware.cpu.lang.contracts.WeakContract;
+import sneer.bricks.hardware.cpu.threads.Threads;
+import sneer.bricks.hardware.io.log.Logger;
 import sneer.bricks.hardware.io.prevalence.nature.Prevalent;
 import sneer.bricks.software.folderconfig.FolderConfig;
 import sneer.foundation.brickness.ClassDefinition;
 import sneer.foundation.lang.Producer;
 
 class PrevalentImpl implements Prevalent {
+	
+	private final List<Prevayler> _prevaylers = new ArrayList<Prevayler>();
+
+	@SuppressWarnings("unused")	private final WeakContract _refToAvoidGc;
+
+
+	{
+		_refToAvoidGc = my(Threads.class).crashing().addPulseReceiver(new Runnable() { @Override public void run() {
+			crash();
+		}});
+	}
+	
 	
 	@Override
 	public List<ClassDefinition> realize(ClassDefinition classDef) {
@@ -26,6 +42,7 @@ class PrevalentImpl implements Prevalent {
 	@Override
 	public <T> T instantiate(Class<T> brick, Class<T> implClass,	Producer<T> producer) {
 		Prevayler prevayler = createPrevayler(producer.produce(), prevalenceBase(brick));
+		_prevaylers.add(prevayler);
 		return (T)Bubble.wrapStateMachine(prevayler);
 	}
 
@@ -56,5 +73,19 @@ class PrevalentImpl implements Prevalent {
 		factory.configureJournalSerializer("xstreamjournal", new SerializerWithClassLoader(PrevalentImpl.class.getClassLoader()));
 		return factory;
 	}
+
 	
+	private void crash() {
+		for (Prevayler prevayler : _prevaylers) crash(prevayler);
+		_prevaylers.clear();
+	}
+
+
+	private void crash(Prevayler prevayler) {
+		try {
+			prevayler.close();
+		} catch (IOException e) {
+			my(Logger.class).log("Exception closing prevayler: " + e);
+		}
+	}
 }
