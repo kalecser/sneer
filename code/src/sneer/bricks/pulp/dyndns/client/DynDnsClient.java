957f48afd6001080bf82009d5f7b60e9aebb70fe
Klaus <klauswuestefeld@gmail.com>
2010-01-06 03:55:14 -0200

    DynDnsClient brick disabled with comment.

--- code/src/sneer/bricks/pulp/dyndns/client/DynDnsClient.java
+++ code/src/sneer/bricks/pulp/dyndns/client/DynDnsClient.java
@@ -1,8 +1,9 @@
 package sneer.bricks.pulp.dyndns.client;
 
-import sneer.foundation.brickness.Brick;
 
-@Brick
-public interface DynDnsClient {
 
-}
+//@Brick
+//Fix: Running on more than one machine will cause redundant (abusive) updates.
+// Stop persisting last discovered IP. Do a DNS lookup every time instead.
+
+public interface DynDnsClient {}
