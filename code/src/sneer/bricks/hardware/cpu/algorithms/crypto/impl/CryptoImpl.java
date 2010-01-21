11cffd0c55d4529c94970690cbd546999b2eefa7
igorarouca <igor.arouca@gmail.com>
2010-01-21 19:36:43 -0200

    FileDigest wiped off the face of the earth...

--- code/src/sneer/bricks/hardware/cpu/algorithms/crypto/impl/CryptoImpl.java
+++ code/src/sneer/bricks/hardware/cpu/algorithms/crypto/impl/CryptoImpl.java
@@ -1,6 +1,9 @@
 package sneer.bricks.hardware.cpu.algorithms.crypto.impl;
 
+import static sneer.foundation.environments.Environments.my;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.Security;
@@ -11,8 +14,11 @@ import org.bouncycastle.util.encoders.Hex;
 import sneer.bricks.hardware.cpu.algorithms.crypto.Crypto;
 import sneer.bricks.hardware.cpu.algorithms.crypto.Digester;
 import sneer.bricks.hardware.cpu.algorithms.crypto.Sneer1024;
+import sneer.bricks.hardware.cpu.threads.throttle.CpuThrottle;
 
 class CryptoImpl implements Crypto {
+
+	private static final int FILE_BLOCK_SIZE = 1024 * 100;
 
 	static {
 		Security.addProvider(new BouncyCastleProvider()); //Optimize: remove this static dependency. Use Bouncycastle classes directly
@@ -38,8 +44,22 @@ class CryptoImpl implements Crypto {
 
 	
 	@Override
-	public Sneer1024 digest(final File file) throws IOException {
-		return new FileDigest(file, newDigester()).result();
+	public Sneer1024 digest(File file) throws IOException {
+		if (file.isDirectory()) throw new IllegalArgumentException("The parameter cannot be a directory");
+
+		Digester digester = newDigester();
+		FileInputStream input = new FileInputStream(file);
+		try {
+			byte[] block = new byte[FILE_BLOCK_SIZE];
+			for (int numOfBytes = input.read(block); numOfBytes != -1; numOfBytes = input.read(block)) {
+				my(CpuThrottle.class).yield();
+				digester.update(block, 0, numOfBytes);
+			}
+		} finally {
+			try { input.close(); } catch (Throwable ignore) {}
+		}
+
+		return digester.digest();
 	}
 
 
