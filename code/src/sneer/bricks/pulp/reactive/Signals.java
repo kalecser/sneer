623aa16d97114e12a3eac13a3a53978edf128378
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2010-02-22 00:20:22 -0300

    Gui-pluggable contact seals: work in progress.

--- code/src/sneer/bricks/pulp/reactive/Signals.java
+++ code/src/sneer/bricks/pulp/reactive/Signals.java
@@ -3,6 +3,9 @@ package sneer.bricks.pulp.reactive;
 import sneer.foundation.brickness.Brick;
 import sneer.foundation.lang.Consumer;
 import sneer.foundation.lang.Functor;
+import sneer.foundation.lang.FunctorX;
+import sneer.foundation.lang.PickyConsumer;
+import sneer.foundation.lang.exceptions.Refusal;
 
 @Brick
 public interface Signals {  
@@ -14,5 +17,8 @@ public interface Signals {
 
 	<A, B> Signal<B> adapt(Signal<A> input, Functor<A, B> functor);
 	<A, B> Signal<B> adaptSignal(Signal<A> input, Functor<A, Signal<B>> functor);
+
+	<A, B> Consumer<A> adapt(Consumer<B> delegate, Functor<A, B> functor);
+	<A, B> PickyConsumer<A> adapt(PickyConsumer<B> delegate, FunctorX<A, B, Refusal> functor);
 
 }
