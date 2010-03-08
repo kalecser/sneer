d6de457f9a0788edf007a3d71cae7ecca7321637
igorarouca <igor.arouca@gmail.com>
2010-03-08 17:54:23 -0300

    WusicTest clean-up

--- code/src/dfcsantos/wusic/tests/WusicTest.java
+++ code/src/dfcsantos/wusic/tests/WusicTest.java
@@ -8,8 +8,7 @@ import java.io.IOException;
 import org.junit.Test;
 
 import sneer.bricks.expression.files.server.FileServer;
-import sneer.bricks.hardware.clock.Clock;
-import sneer.bricks.hardware.cpu.threads.Threads;
+import sneer.bricks.hardware.clock.ticker.custom.CustomClockTicker;
 import sneer.bricks.hardware.io.IO;
 import sneer.bricks.pulp.blinkinglights.BlinkingLights;
 import sneer.bricks.pulp.reactive.Signal;
@@ -18,7 +17,6 @@ import sneer.bricks.pulp.tuples.TupleSpace;
 import sneer.bricks.software.folderconfig.tests.BrickTest;
 import sneer.foundation.environments.Environment;
 import sneer.foundation.environments.Environments;
-import sneer.foundation.lang.Closure;
 import sneer.foundation.lang.ClosureX;
 import dfcsantos.wusic.Wusic;
 import dfcsantos.wusic.Wusic.OperatingMode;
@@ -34,53 +32,51 @@ public class WusicTest extends BrickTest {
 	public void basicStuff() {
 		_subject1 = my(Wusic.class);
 
-		assertSignalValue(_subject1.sharedTracksFolder(), new File(tmpFolder(), "data/media/tracks"));
+		waitForSignalValue(_subject1.sharedTracksFolder(), new File(tmpFolder(), "data/media/tracks"));
 
-		assertSignalValue(_subject1.operatingMode(), OperatingMode.OWN);
+		waitForSignalValue(_subject1.operatingMode(), OperatingMode.OWN);
 		_subject1.setOperatingMode(OperatingMode.PEERS);
-		assertSignalValue(_subject1.operatingMode(), OperatingMode.PEERS);
+		waitForSignalValue(_subject1.operatingMode(), OperatingMode.PEERS);
 
 		_subject1.start();
 		if (!my(BlinkingLights.class).lights().currentGet(0).caption().equals("No Tracks Found")) fail();
-		assertSignalValue(_subject1.playingTrack(), null);
-		assertSignalValue(_subject1.isPlaying(), false);
+		waitForSignalValue(_subject1.playingTrack(), null);
+		waitForSignalValue(_subject1.isPlaying(), false);
 
-		assertSignalValue(_subject1.numberOfPeerTracks(), 0);
-		assertSignalValue(_subject1.isTrackDownloadActive(), false);
+		waitForSignalValue(_subject1.numberOfPeerTracks(), 0);
+		waitForSignalValue(_subject1.isTrackDownloadActive(), false);
 
 		_subject1.trackDownloadActivator().consume(true);
 
-		assertSignalValue(_subject1.isTrackDownloadActive(), true);		
-		assertSignalValue(_subject1.trackDownloadAllowance(), Wusic.DEFAULT_TRACKS_DOWNLOAD_ALLOWANCE);
+		waitForSignalValue(_subject1.isTrackDownloadActive(), true);		
+		waitForSignalValue(_subject1.trackDownloadAllowance(), Wusic.DEFAULT_TRACKS_DOWNLOAD_ALLOWANCE);
 	}
 
 	@Test (timeout = 6000)
 	public void peersMode() throws IOException {
-//		LoggerMocks.showLog = true;
+		my(CustomClockTicker.class).start(10, 2000);
 
-		Environment remoteEnvironment = newTestEnvironment(my(TupleSpace.class), my(Clock.class));
+		Environment remoteEnvironment = newTestEnvironment(my(TupleSpace.class));
 		configureStorageFolder(remoteEnvironment, "remote/data");
 		configureTmpFolder(remoteEnvironment, "remote/tmp");
 
 		final String[] trackNames = new String[] { "track1.mp3", "track2.mp3", "track3.mp3" };
 		Environments.runWith(remoteEnvironment, new ClosureX<IOException>() { @Override public void run() throws IOException {
+			my(CustomClockTicker.class).start(10, 2000);
+
 			createSampleTracks(trackNames);
 			assertEquals(3, sharedTracksFolder().listFiles().length);
 
 			my(FileServer.class);
+
 			_subject2 = my(Wusic.class);
 			_subject2.trackDownloadActivator().consume(true);
-//			my(TupleLogger.class);
 		}});
 
-//		my(TupleLogger.class);
 		_subject1 = my(Wusic.class);
 		_subject1.trackDownloadActivator().consume(true);
 
-		while(true) {
-			accelerateTrackEndorsements(remoteEnvironment);
-			if (_subject1.numberOfPeerTracks().currentValue() == 3) break;
-		}
+		waitForSignalValue(_subject1.numberOfPeerTracks(), 3);
 
 //		checking(new Expectations() {{
 //			oneOf(_trackPlayer).startPlaying(with(any(Track.class)), with(any(Signal.class)), with(any(Runnable.class)));
@@ -88,21 +84,14 @@ public class WusicTest extends BrickTest {
 //
 //		_subject1.setOperatingMode(OperatingMode.PEERS);
 //		_subject1.start();
-//		assertSignalValue(_subject1.isPlaying(), true);
+//		waitForSignalValue(_subject1.isPlaying(), true);
 //		_subject1.pauseResume();
-//		assertSignalValue(_subject1.isPlaying(), false);
+//		waitForSignalValue(_subject1.isPlaying(), false);
 
 		crash(remoteEnvironment);
 	}
 
-	private void accelerateTrackEndorsements(Environment environment) {
-		Environments.runWith(environment, new Closure() { @Override public void run() {
-			my(Clock.class).advanceTime(2000);
-			my(Threads.class).sleepWithoutInterruptions(20);
-		}});
-	}
-
-	private <T> void assertSignalValue(Signal<T> signal, T value) {
+	private <T> void waitForSignalValue(Signal<T> signal, T value) {
 		my(SignalUtils.class).waitForValue(signal, value);
 	}
 
