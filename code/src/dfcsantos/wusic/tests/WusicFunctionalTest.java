ded5d95beeb6b8601b2e914da28299f7a5a2e539
igorarouca <igor.arouca@gmail.com>
2010-03-10 19:19:21 -0300

    WusicTest renamed to WusicFunctionalTest, and refactored

--- code/src/dfcsantos/wusic/tests/WusicFunctionalTest.java
+++ code/src/dfcsantos/wusic/tests/WusicFunctionalTest.java
@@ -4,16 +4,15 @@ import static sneer.foundation.environments.Environments.my;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
 import org.jmock.Expectations;
 import org.junit.Test;
 
-import sneer.bricks.expression.files.map.FileMap;
-import sneer.bricks.expression.files.server.FileServer;
+import scala.actors.threadpool.Arrays;
 import sneer.bricks.hardware.clock.ticker.custom.CustomClockTicker;
 import sneer.bricks.hardware.io.IO;
+import sneer.bricks.hardware.ram.collections.CollectionUtils;
 import sneer.bricks.pulp.blinkinglights.BlinkingLights;
 import sneer.bricks.pulp.reactive.Signal;
 import sneer.bricks.pulp.reactive.SignalUtils;
@@ -23,13 +22,13 @@ import sneer.foundation.brickness.testsupport.Bind;
 import sneer.foundation.environments.Environment;
 import sneer.foundation.environments.Environments;
 import sneer.foundation.lang.ClosureX;
+import sneer.foundation.lang.Functor;
 import dfcsantos.tracks.Track;
-import dfcsantos.tracks.Tracks;
 import dfcsantos.tracks.execution.player.TrackPlayer;
 import dfcsantos.wusic.Wusic;
 import dfcsantos.wusic.Wusic.OperatingMode;
 
-public class WusicTest extends BrickTest {
+public class WusicFunctionalTest extends BrickTest {
 
 	private Wusic _subject1;
 	private Wusic _subject2;
@@ -107,9 +106,10 @@ public class WusicTest extends BrickTest {
 	@Test (timeout = 2000)
 	public void ownModeWithMultipleTracks() throws IOException {
 		/*	Folder structure created:
-		 * 
+		 *
 		 *	tmpFolder()
 		 * 		|_ tmp/media/tracks (Default Playing Folder)
+		 * 				|_ music.mp3
 		 * 				|_ subdirectory1
 		 * 						|_ track1.mp3
 		 * 						|_ track2.mp3
@@ -148,7 +148,7 @@ public class WusicTest extends BrickTest {
 		assertEquals("track5", playingTrack());
 		_subject1.skip();
 		assertEquals("track6", playingTrack());
-		_subject1.skip(); // Back to first track
+		_subject1.skip(); // Playlist reloaded
 		assertEquals("track1", playingTrack());
 
 		// Play only the songs from subdirectory1
@@ -160,15 +160,24 @@ public class WusicTest extends BrickTest {
 		_subject1.skip();
 		assertEquals("track1", playingTrack());
 
+		// Play all songs randomly (pseudo-random done by regression)
 		_subject1.setPlayingFolder(rootFolder);
 		_subject1.setShuffle(true);
 
-		Track previousTrack = null;
-		do {
-			previousTrack = _subject1.playingTrack().currentValue();
-			_subject1.skip();
-		} while(areSequential(previousTrack, _subject1.playingTrack().currentValue()));
-
+		_subject1.skip();
+		assertEquals("track5", playingTrack());
+		_subject1.skip();
+		assertEquals("track2", playingTrack());
+		_subject1.skip();
+		assertEquals("track3", playingTrack());
+		_subject1.skip();
+		assertEquals("track6", playingTrack());
+		_subject1.skip();
+		assertEquals("track4", playingTrack());
+		_subject1.skip();
+		assertEquals("track1", playingTrack());
+		_subject1.skip(); // Playlist reloaded
+		assertEquals("track5", playingTrack());
 	}
 
 	@Test (timeout = 4000)
@@ -187,28 +196,22 @@ public class WusicTest extends BrickTest {
 
 		// Deletes the first played track
 		_subject1.start();
-		Track deletedTrack = _subject1.playingTrack().currentValue();
 		_subject1.deleteTrack(); // Skip is called automatically after a track is deleted
-		assertNull(my(FileMap.class).getHash(deletedTrack.file()));
 
 		// Keeps the last two played tracks
-		List<String> keptTracks = new ArrayList<String>();
-		keptTracks.add(_subject1.playingTrack().currentValue().name());
+		File[] keptTracks = new File[2];
+		keptTracks[0] = _subject1.playingTrack().currentValue().file();
 		_subject1.meToo();
 		assertTrue(_subject1.isPlaying().currentValue()); // MeToo doesn't affect the playing track's flow
 		_subject1.skip();
-		keptTracks.add(_subject1.playingTrack().currentValue().name());
+		keptTracks[1] = _subject1.playingTrack().currentValue().file();
 		_subject1.meToo();
-
-		_subject1.stop();
 
 		waitForSignalValue(_subject1.numberOfPeerTracks(), 0);
 
-		List<Track> sharedTracks = my(Tracks.class).listTracksFromFolder(sharedTracksFolder()); 
-		for (Track sharedTrack : sharedTracks) {
-			assertTrue(keptTracks.contains(sharedTrack.name()));
-			assertNotNull(my(FileMap.class).getHash(sharedTrack.file()));
-		}
+		File[] sharedTracks = sharedTracksFolder().listFiles();
+		assertEquals(2, sharedTracks.length);
+		assertElementsInAnyOrder(trackNames(keptTracks), trackNames(sharedTracks).toArray(new String[0]));
 
 		crash(remoteEnvironment);
 	}
@@ -226,14 +229,6 @@ public class WusicTest extends BrickTest {
 		return _subject1.playingTrack().currentValue().name();
 	}
 
-	private boolean areSequential(Track previousTrack, Track currentTrack) {
-		return lastCharOf(currentTrack.name()) - lastCharOf(previousTrack.name()) == 1;
-	}
-
-	private char lastCharOf(String string) {
-		return string.charAt(string.length() - 1);
-	}
-
 	private void downloadTracksFrom(Environment remoteEnvironment) throws IOException {
 		configureStorageFolder(remoteEnvironment, "remote/data");
 		configureTmpFolder(remoteEnvironment, "remote/tmp");
@@ -244,8 +239,6 @@ public class WusicTest extends BrickTest {
 			createSampleTracks(sharedTracksFolder(), new String[] { "track1.mp3", "track2.mp3", "track3.mp3" });
 			assertEquals(3, sharedTracksFolder().listFiles().length);
 
-			my(FileServer.class);
-
 			_subject2 = my(Wusic.class);
 			_subject2.trackDownloadActivator().consume(true);
 		}});
@@ -253,6 +246,15 @@ public class WusicTest extends BrickTest {
 		_subject1.trackDownloadActivator().consume(true);
 
 		waitForSignalValue(_subject1.numberOfPeerTracks(), 3);
+	}
+
+	private Collection<String> trackNames(File[] trackFiles) {
+		return my(CollectionUtils.class).map(
+			Arrays.asList(trackFiles),
+			new Functor<File, String>() { @Override public String evaluate(File trackFile) throws RuntimeException {
+				return trackFile.getName();
+			}}
+		);
 	}
 
 	private File sharedTracksFolder() {
