65cc8bffe3c1f3b7a869cb690bb2b828c2b034ec
igorarouca <igor.arouca@gmail.com>
2010-03-10 19:20:54 -0300

    RandomPlaylist using random source with clock-time seed

--- code/src/dfcsantos/tracks/execution/playlist/impl/RandomPlaylist.java
+++ code/src/dfcsantos/tracks/execution/playlist/impl/RandomPlaylist.java
@@ -1,8 +1,13 @@
 package dfcsantos.tracks.execution.playlist.impl;
 
+import static sneer.foundation.environments.Environments.my;
+
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
+
+import sneer.bricks.hardware.clock.Clock;
 
 class RandomPlaylist extends AbstractPlaylist {
 
@@ -12,7 +17,7 @@ class RandomPlaylist extends AbstractPlaylist {
 
 	@Override
 	public void sortTracks(List<File> tracks) {
-		Collections.shuffle(tracks);
+		Collections.shuffle(tracks, new Random(my(Clock.class).time().currentValue()));
 	}
 
 }
