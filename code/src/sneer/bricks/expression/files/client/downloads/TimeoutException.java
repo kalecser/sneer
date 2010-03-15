770d8c52bec16b7db1dd2b613df893ecf4117cec
Patrick Roemer <quellkristfalconer@gmail.com>
2010-03-15 20:18:08 +0100

    timeout exception message, cache received contents in folder download

--- code/src/sneer/bricks/expression/files/client/downloads/TimeoutException.java
+++ code/src/sneer/bricks/expression/files/client/downloads/TimeoutException.java
@@ -1,3 +1,10 @@
 package sneer.bricks.expression.files.client.downloads;
 
-public class TimeoutException extends Exception {}
+public class TimeoutException extends Exception {
+	public TimeoutException() {		
+	}
+
+	public TimeoutException(String msg) {
+		super(msg);
+	}
+}
