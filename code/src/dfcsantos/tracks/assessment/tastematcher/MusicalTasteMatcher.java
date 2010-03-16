6ab66d9208bda5e4452a29a705ef91b29b54afb6
igorarouca <igor.arouca@gmail.com>
2010-03-16 15:54:46 -0300

    MusicalTasteMatcher.bestMatch()

--- code/src/dfcsantos/tracks/assessment/tastematcher/MusicalTasteMatcher.java
+++ code/src/dfcsantos/tracks/assessment/tastematcher/MusicalTasteMatcher.java
@@ -1,5 +1,6 @@
 package dfcsantos.tracks.assessment.tastematcher;
 
+import sneer.bricks.network.social.Contact;
 import sneer.bricks.pulp.reactive.Signal;
 import sneer.foundation.brickness.Brick;
 
@@ -7,5 +8,7 @@ import sneer.foundation.brickness.Brick;
 public interface MusicalTasteMatcher {
 
 	void setOnOffSwitch(Signal<Boolean> onOffSwitch);
+
+	Signal<Contact> bestMatch();
 
 }
--- code/src/dfcsantos/tracks/assessment/tastematcher/impl/MusicalTasteMatcherImpl.java
+++ code/src/dfcsantos/tracks/assessment/tastematcher/impl/MusicalTasteMatcherImpl.java
@@ -4,6 +4,7 @@ import static sneer.foundation.environments.Environments.my;
 
 import java.io.File;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 
 import sneer.bricks.hardware.cpu.lang.contracts.WeakContract;
@@ -12,7 +13,9 @@ import sneer.bricks.hardware.ram.ref.immutable.ImmutableReference;
 import sneer.bricks.hardware.ram.ref.immutable.ImmutableReferences;
 import sneer.bricks.network.social.Contact;
 import sneer.bricks.network.social.Contacts;
+import sneer.bricks.pulp.reactive.Register;
 import sneer.bricks.pulp.reactive.Signal;
+import sneer.bricks.pulp.reactive.Signals;
 import sneer.foundation.lang.Consumer;
 import dfcsantos.tracks.assessment.TrackAssessment;
 import dfcsantos.tracks.assessment.assessor.TrackAssessor;
@@ -22,12 +25,19 @@ import dfcsantos.tracks.storage.folder.TracksFolderKeeper;
 class MusicalTasteMatcherImpl implements MusicalTasteMatcher {
 
 	private final Map<Contact, Integer> _scoreByContact = new ConcurrentHashMap<Contact, Integer>();
+	private final Register<Contact> _topScorer = my(Signals.class).newRegister(randomContact());
 
 	private final ImmutableReference<Signal<Boolean>> _onOffSwitch = my(ImmutableReferences.class).newInstance();
 
 	private WeakContract _assessmentsConsumerContract;
 
 	@SuppressWarnings("unused") private WeakContract _toAvoidGC;
+
+	private static Contact randomContact() {
+		// Fix: think of the best way to initialize _topScorer
+		return null;
+//		return my(Contacts.class).contacts().currentElements().iterator().next();
+	}
 
 	@Override
 	public void setOnOffSwitch(Signal<Boolean> onOffSwitch) {
@@ -36,6 +46,11 @@ class MusicalTasteMatcherImpl implements MusicalTasteMatcher {
 		_toAvoidGC = onOffSwitch.addReceiver(new Consumer<Boolean>() { @Override public void consume(Boolean isOn) {
 			react(isOn);
 		}});
+	}
+
+	@Override
+	public Signal<Contact> bestMatch() {
+		return _topScorer.output();
 	}
 
 	private void react(boolean isOn) {
@@ -66,6 +81,7 @@ class MusicalTasteMatcherImpl implements MusicalTasteMatcher {
 			return;
 		}
 		_scoreByContact.put(peer, assessment.score());
+		updateTopScorerIfNecessary();
 	}
 
 	private Contact assessedPeer(TrackAssessment assessment) {
@@ -76,6 +92,28 @@ class MusicalTasteMatcherImpl implements MusicalTasteMatcher {
 
 	private File sharedTracksFolder() {
 		return my(TracksFolderKeeper.class).sharedTracksFolder().currentValue();
+	}
+
+	private void updateTopScorerIfNecessary() {
+		Contact topScorer = topScorer();
+		if (topScorer == null) return;
+		_topScorer.setter().consume(topScorer);
+	}
+
+	private Contact topScorer() {
+		Contact topScorer = null;
+		Integer topScore = topScore();
+		if (topScore == null) topScore = Integer.MIN_VALUE;
+		for(Entry<Contact, Integer> scoreByContact : _scoreByContact.entrySet()) {
+			int score = scoreByContact.getValue();
+			if (score > topScore)
+				topScorer = scoreByContact.getKey();
+		}
+		return topScorer;
+	}
+
+	private Integer topScore() {
+		return _scoreByContact.get(_topScorer.output().currentValue());
 	}
 
 }
