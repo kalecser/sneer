c957a52cfe1b5c0c67bce8921f09c6d1564233cd
igorarouca <igor.arouca@gmail.com>
2010-04-12 16:35:08 -0300

    Reactive Progress Bar implemented using a minimalist approach

--- code/src/sneer/bricks/expression/files/client/downloads/gui/DownloadDetailsPanel.java
+++ code/src/sneer/bricks/expression/files/client/downloads/gui/DownloadDetailsPanel.java
@@ -11,17 +11,17 @@ import javax.swing.JProgressBar;
 import sneer.bricks.expression.files.client.downloads.Download;
 import sneer.bricks.hardware.cpu.lang.Lang;
 import sneer.bricks.hardware.cpu.lang.contracts.WeakContract;
-import sneer.foundation.lang.Consumer;
+import sneer.bricks.skin.widgets.reactive.ReactiveWidgetFactory;
+import sneer.bricks.skin.widgets.reactive.Widget;
 
 class DownloadDetailsPanel extends JPanel {
 
 	private final DownloadsPanel _parent;
 
 	private final JLabel _sourceAndfile;
-	private final JProgressBar _progress;
+	private final Widget<JProgressBar> _progress;
 
 	@SuppressWarnings("unused") private final WeakContract _toAvoidGC;
-	@SuppressWarnings("unused") private final WeakContract _toAvoidGC2;
 
 	DownloadDetailsPanel(DownloadsPanel parent, Download download) {
 		super(new GridLayout(2, 1, 0, 1));
@@ -33,14 +33,10 @@ class DownloadDetailsPanel extends JPanel {
 		_sourceAndfile = new JLabel(source + " :: " + file);
 		add(_sourceAndfile);
 
-		_progress = new JProgressBar(0, 100);
-		_toAvoidGC = download.progress().addReceiver(new Consumer<Integer>() { @Override public void consume(Integer progress) {
-			_progress.setValue(progress);
-		}});
-		_progress.setStringPainted(true);
-		add(_progress);
+		_progress = my(ReactiveWidgetFactory.class).newProgressBar(download.progress());
+		add(_progress.getMainWidget());
 
-		_toAvoidGC2 = download.finished().addPulseReceiver(new Runnable() { @Override public void run() {
+		_toAvoidGC = download.finished().addPulseReceiver(new Runnable() { @Override public void run() {
 			close();
 		}});
 	}
