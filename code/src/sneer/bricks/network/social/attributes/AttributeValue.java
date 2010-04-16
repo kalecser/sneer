7f07d5fd25de7b3a5ac43e390c86fef341a459ae
igorarouca <igor.arouca@gmail.com>
2010-04-16 19:58:29 -0300

    AttributeValue.serializedValue: byte[] --> ImmutableByteArray

--- code/src/sneer/bricks/network/social/attributes/AttributeValue.java
+++ code/src/sneer/bricks/network/social/attributes/AttributeValue.java
@@ -1,14 +1,15 @@
 package sneer.bricks.network.social.attributes;
 
 import sneer.bricks.expression.tuples.Tuple;
+import sneer.bricks.hardware.ram.arrays.ImmutableByteArray;
 
 public class AttributeValue extends Tuple {
 
 	public final String attributeName;
 
-	public final byte[] serializedValue;
+	public final ImmutableByteArray serializedValue;
 
-	public AttributeValue(String attributeName_, byte[] serializedValue_) {
+	public AttributeValue(String attributeName_, ImmutableByteArray serializedValue_) {
 		attributeName = attributeName_;
 		serializedValue = serializedValue_;
 	}
--- code/src/sneer/bricks/network/social/attributes/impl/AttributesImpl.java
+++ code/src/sneer/bricks/network/social/attributes/impl/AttributesImpl.java
@@ -3,6 +3,7 @@ package sneer.bricks.network.social.attributes.impl;
 import static sneer.foundation.environments.Environments.my;
 import sneer.bricks.expression.tuples.TupleSpace;
 import sneer.bricks.hardware.cpu.lang.contracts.WeakContract;
+import sneer.bricks.hardware.ram.arrays.ImmutableByteArray;
 import sneer.bricks.hardware.ram.ref.weak.keeper.WeakReferenceKeeper;
 import sneer.bricks.identity.seals.OwnSeal;
 import sneer.bricks.identity.seals.Seal;
@@ -21,6 +22,10 @@ class AttributesImpl implements Attributes {
 
 	private final ClassLoader apiClassLoader = Attributes.class.getClassLoader();
 
+	{
+		my(TupleSpace.class).keep(AttributeValue.class);
+	}
+
 	@Override
 	public <T> Signal<T> myAttributeValue(final Class<? extends Attribute<T>> attribute) {
 		return attributeValueFor(my(OwnSeal.class).get(), attribute);
@@ -28,7 +33,7 @@ class AttributesImpl implements Attributes {
 
 	@Override
 	public <T> Signal<T> attributeValueFor(final Contact contact, final Class<? extends Attribute<T>> attribute) {
-		return attributeValueFor(my(ContactSeals.class).sealGiven(contact).currentValue(), attribute);
+		return attributeValueFor(my(ContactSeals.class).sealGiven(contact), attribute);
 	}
 
 	@Override
@@ -38,20 +43,21 @@ class AttributesImpl implements Attributes {
 		}};
 	}
 
-	private <T> Signal<T> attributeValueFor(final Seal party, final Class<? extends Attribute<T>> attribute) {
+	private <T> Signal<T> attributeValueFor(final Signal<Seal> partySignal, final Class<? extends Attribute<T>> attribute) {
 		final Register<T> result = my(Signals.class).newRegister(null);
 		WeakContract toAvoidGC = my(TupleSpace.class).addSubscription(AttributeValue.class, new Consumer<AttributeValue>() { @Override public void consume(AttributeValue tuple) {
 			if (!attribute.getName().equals(tuple.attributeName)) return;
 
-			if (party.equals(tuple.publisher))
-				result.setter().consume((T) deserialize(tuple.serializedValue));
+			Seal party = partySignal.currentValue();
+			if (party != null && party.equals(tuple.publisher))
+				result.setter().consume((T) deserialize(tuple.serializedValue.copy()));
 		}});
 		my(WeakReferenceKeeper.class).keep(result.output(), toAvoidGC);
 		return my(WeakReferenceKeeper.class).keep(result.output(), result);
 	}
 
-	private byte[] serialize(Object value) {
-		return my(Serializer.class).serialize(value);
+	private ImmutableByteArray serialize(Object value) {
+		return new ImmutableByteArray(my(Serializer.class).serialize(value));
 	}
 
 	private Object deserialize(byte[] serializedValue) {
