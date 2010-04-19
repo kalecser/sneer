e82d5c9afa3178225d94a5c54d802019b6970ddc
unknown <reunioes@.(none)>
2010-04-19 18:12:38 -0300

    DirectedGraph.removeEdge() added

--- code/src/sneer/bricks/hardware/ram/graphs/impl/DirectedGraphImpl.java
+++ code/src/sneer/bricks/hardware/ram/graphs/impl/DirectedGraphImpl.java
@@ -1,7 +1,6 @@
 package sneer.bricks.hardware.ram.graphs.impl;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -15,6 +14,7 @@ class DirectedGraphImpl<T> implements DirectedGraph<T> {
 
 	private CacheMap<T, Set<T>> _successorsByVertex = CacheMap.newInstance();
 
+	
 	@Override
 	public void addEdge(T vertex, T successor) {
 		addVertex(successor);
@@ -23,19 +23,28 @@ class DirectedGraphImpl<T> implements DirectedGraph<T> {
 		produceSuccessors(vertex).add(successor);
 	}
 
+	
+	@Override
+	public void removeEdge(T vertex, T successor) {
+		produceSuccessors(vertex).remove(successor);
+	}
+
+
 	@Override
 	public void addVertex(T successor) {
 		produceSuccessors(successor); //Registers the vertex in the _successorsByVertex map
 	}
 
+	
 	private Set<T> produceSuccessors(final T payload) {
 		return _successorsByVertex.get(payload, new Producer<Set<T>>() { @Override public Set<T> produce() {
 			return new HashSet<T>();
 		}});
 	}
 
+	
 	@Override
-	public Collection<T> detectCycle() {
+	public List<T> detectCycle() {
 		ArrayList<T> result = new ArrayList<T>();
 		
 		for (T vertex : vertices())
@@ -44,12 +53,13 @@ class DirectedGraphImpl<T> implements DirectedGraph<T> {
 		return result;
 	}
 
+	
 	private boolean detectCycle(T vertex, List<T> cycle) {
 		if (cycle.contains(vertex)) {
 			removeVerticesVisitedBeforeEnteringCycle(vertex, cycle);
 			return true;
 		}
-		
+	
 		cycle.add(vertex);
 		
 		for (T successor : produceSuccessors(vertex))
@@ -59,12 +69,14 @@ class DirectedGraphImpl<T> implements DirectedGraph<T> {
 		return false;
 	}
 
+	
 	private void removeVerticesVisitedBeforeEnteringCycle(T vertexThatStartedCycle, List<T> cycle) {
 		Iterator<T> it = cycle.iterator();
 		while (it.hasNext() && !it.next().equals(vertexThatStartedCycle))
 			it.remove();
 	}
 
+	
 	@Override
 	public T pluck() {
 		if (vertices().isEmpty()) return null;
@@ -76,6 +88,7 @@ class DirectedGraphImpl<T> implements DirectedGraph<T> {
 		throw new IllegalStateException("Unable to pluck a leaf vertex because this graph has cycles.");
 	}
 
+	
 	private T pluck(T vertex) {
 		for (T otherVertex : vertices())
 			produceSuccessors(otherVertex).remove(vertex);
@@ -84,6 +97,7 @@ class DirectedGraphImpl<T> implements DirectedGraph<T> {
 		return vertex;
 	}
 
+	
 	private Set<T> vertices() {
 		return _successorsByVertex.keySet();
 	}
