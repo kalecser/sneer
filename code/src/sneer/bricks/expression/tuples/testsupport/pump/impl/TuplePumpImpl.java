370a148935274729b42276682dda2a39a027f16b
igorarouca <igor.arouca@gmail.com>
2010-04-27 14:35:46 -0300

    Comments added

--- code/src/sneer/bricks/expression/tuples/testsupport/pump/impl/TuplePumpImpl.java
+++ code/src/sneer/bricks/expression/tuples/testsupport/pump/impl/TuplePumpImpl.java
@@ -14,19 +14,19 @@ import sneer.foundation.lang.Producer;
 
 class TuplePumpImpl implements TuplePump {
 
-	private final Environment _tupleWell1;
-	private final Environment _tupleWell2;
+	private final Environment _well1;
+	private final Environment _well2;
 
 	private final WeakContract _pipe1;
 	private final WeakContract _pipe2;
 
 
-	public TuplePumpImpl(Environment well1, Environment well2) {
-		_tupleWell1 = well1;
-		_tupleWell2 = well2;
+	public TuplePumpImpl(Environment aTupleWell, Environment anotherTupleWell) {
+		_well1 = aTupleWell;
+		_well2 = anotherTupleWell;
 
-		_pipe1 = pipe(well1, well2);
-		_pipe2 = pipe(well2, well1);
+		_pipe1 = pipe(_well1, _well2); // Connects well1 to well2 so that tuples received by the first are pumped to the second
+		_pipe2 = pipe(_well2, _well1); // Connects well2 to well1 to form a two-way connection
 	}
 
 
@@ -43,8 +43,8 @@ class TuplePumpImpl implements TuplePump {
 
 	@Override
 	public void waitForAllDispatchingToFinish() {
-		waitForAllDispatchingToFinishIn(_tupleWell1);
-		waitForAllDispatchingToFinishIn(_tupleWell2);
+		waitForAllDispatchingToFinishIn(_well1);
+		waitForAllDispatchingToFinishIn(_well2);
 	}
 
 
@@ -60,6 +60,5 @@ class TuplePumpImpl implements TuplePump {
 		_pipe1.dispose();
 		_pipe2.dispose();
 	}
-
 
 }
