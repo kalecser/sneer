333b91de79e9df172040102e23574d00c1ddebb3
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2010-04-26 23:06:20 -0300

    Better exception logging

--- code/src/sneer/bricks/hardware/io/log/exceptions/impl/ExceptionLoggerImpl.java
+++ code/src/sneer/bricks/hardware/io/log/exceptions/impl/ExceptionLoggerImpl.java
@@ -11,14 +11,14 @@ class ExceptionLoggerImpl implements ExceptionLogger {
 
 	@Override
 	public void log(Throwable throwable) {
-		log(throwable, "{} thrown: {}", throwable.getClass(), throwable.getMessage());
+		log(throwable, "{} thrown.", throwable.getClass());
 	}
 
 	
 	@Override
 	public void log(Throwable throwable, String message, Object... messageInsets) {
 		leakIfNecessary(throwable);
-		message += "/n" + my(StackTraceLogger.class).stackToString(throwable);
+		message += "\n" + my(StackTraceLogger.class).stackToString(throwable);
 		my(Logger.class).log(message, messageInsets);
 	}
 
