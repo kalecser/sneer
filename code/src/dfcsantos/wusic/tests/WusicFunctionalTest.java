9b12a5036929114a52e662c481e4962728972c72
igorarouca <igor.arouca@gmail.com>
2010-05-12 18:46:47 -0300

    Shorter timeout limits (testing server)

--- code/src/dfcsantos/wusic/tests/WusicFunctionalTest.java
+++ code/src/dfcsantos/wusic/tests/WusicFunctionalTest.java
@@ -76,7 +76,7 @@ public class WusicFunctionalTest extends BrickTest {
 	}
 
 	
-	@Test (timeout = 4000)
+	@Test (timeout = 3000)
 	public void ownModeWithOneTrack() throws IOException {
 		_subject1 = my(Wusic.class);
 		createSampleTracks(_subject1.playingFolder(), "track1.mp3");
@@ -199,7 +199,7 @@ public class WusicFunctionalTest extends BrickTest {
 	}
 
 	
-	@Test (timeout = 6000)
+	@Test (timeout = 4000)
 	public void peersMode() throws IOException {
 		Environment remote = configureRemoteEnvironment();
 		makeFriendsWith(remote);
