cacc0576baa72c0a5fcca7ba2e2c99c008260e19
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2010-05-13 02:09:53 -0300

    WindGui disabled until it is fixed. Enter bug is too frustrating.

--- code/src/sneer/bricks/snapps/wind/gui/WindGui.java
+++ code/src/sneer/bricks/snapps/wind/gui/WindGui.java
@@ -1,9 +1,8 @@
 package sneer.bricks.snapps.wind.gui;
 
 import sneer.bricks.skin.main.instrumentregistry.Instrument;
-import sneer.bricks.software.bricks.snappstarter.Snapp;
 import sneer.foundation.brickness.Brick;
 
-@Snapp
+//@Snapp
 @Brick
 public interface WindGui extends Instrument {}
