3171aae20945c418d3f3b6167a9be2bc95c5381e
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2010-05-17 17:59:18 -0300

    Rene crypto spike.

--- spikes/src/spikes/rene/Krypton/KryptonFile.java
+++ spikes/src/spikes/rene/Krypton/KryptonFile.java
@@ -1,4 +1,7 @@
+package spikes.rene.Krypton;
+
 import java.util.Random;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -6,53 +9,53 @@ import java.io.IOException;
 public class KryptonFile {
 
 	public static void main (String args[]) throws IOException {
-		Random random = new Random();
+	
+		System.out.print("Krypton, criptografador de arquivos enormes\nby -=ReNeX=-\n\nInicializando... ");
+		
 		String filepath=args[0], pass=args[1];
-		if (filepath.length()==0) return;
+		if (filepath.length()==0) {System.out.print("Erro: Arquivo vazio."); return;}
+		String filetemp=filepath.substring(0,filepath.indexOf('.'))+".tmp";
 
-		int seed=0, i=0;
+		int seed=0, i=0, i2=0;
+		Random rand = new Random();
 		for (i=0; i<pass.length(); i++)
 			seed+=pass.charAt(i)*(i+1);
-		random.setSeed(seed);
+		rand.setSeed(seed);
 		
 		FileInputStream fr = new FileInputStream(filepath);
+		FileOutputStream fw = new FileOutputStream(filetemp);
+		
+		int len=10000000, max=fr.available(), iter=(int) Math.floor(max/len), resto=max-iter*len;
+		byte[] chars=new byte[len];
+		
+		String sizetag;
+		if (max<1024) sizetag=max+" bytes";
+		else if (max<1048576) sizetag=(max/1024f)+" KB";
+		else sizetag=(max/1048576f)+" MB";
+		
+		System.out.print("pronto.\nArquivo: "+filepath.substring(filepath.indexOf('\\')+1,filepath.length())+", Tamanho total: "+sizetag+".\nKrypton-grafando arquivo... ");
+		
+		for (i=0; i<iter; i++) {
+			fr.read(chars);
+			for (i2=0; i2<len; i2++) 
+				chars[i2]=(byte) (chars[i2]^rand.nextInt(256));
+			fw.write(chars);
+		}
+		fr.read(chars,0,resto);
+		for (i=0; i<resto; i++) 
+			chars[i]=(byte) (chars[i]^rand.nextInt(256));
+		fw.write(chars, 0, resto);
 
-		System.out.print("Lendo arquivo... ");
-		int max=fr.available();
-		int num=(int) Math.floor(max/1000000);
-		int resto=max-num*1000000;
-		byte chars[][]=new byte[num][];
-		
-		
-		for (i=0; i<=num; i++){
-			chars[i]=new byte[1000000];	
-			fr.read(chars[i],i*1000000,1000000);
-		}
-		chars[i]=new byte[resto];	
-		fr.read(chars[i],i*1000000,resto);
+		System.out.print("pronto.\nFinalizando... ");
+	
 		fr.close();
-		
-		System.out.print("completo.\nKriptografando arquivo... ");
-		
-		for (i=0; i<num; i++) {
-			for (int i2=0; i2<1000000; i2++) 
-				chars[i][i2]=(byte) (chars[i][i2]^random.nextInt(256));
-		}
-		for (int i2=0; i2<resto; i2++) 
-			chars[i][i2]=(byte) (chars[i][i2]^random.nextInt(256));
-		
-		System.out.print("completo.\nSalvando arquivo... ");
-		FileOutputStream fw = new FileOutputStream(filepath);
-		
-		for (i=0; i<num; i++) {
-			for (int i2=0; i2<1000000; i2++) 
-				fw.write(chars[i][i2]);
-		}
-		for (int i2=0; i2<resto; i2++) 
-			fw.write(chars[i][i2]);
-		
-		
 		fw.close();
-		System.out.print("completo.");
+		File oldfile=new File(filepath);
+		File tmpfile=new File(filetemp);
+		oldfile.renameTo(new File(filepath.substring(0,filepath.indexOf('.'))+".bkp")); 
+		oldfile.delete();
+		tmpfile.renameTo(new File(filepath));
+
+		System.out.print("pronto.");
 	}
 }
