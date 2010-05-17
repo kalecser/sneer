274dda7294ef328866335a0f73e776b421a69362
unknown <ReNeX@.(none)>
2010-05-17 00:29:37 -0300

    test... may become useful

---
+++ spikes/src/spikes/rene/Krypton/KryptonFile.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class KryptonFile {
+
+	public static void main (String args[]) throws IOException {
+		Random random = new Random();
+		String filepath=args[0], pass=args[1];
+		if (filepath.length()==0) return;
+
+		int seed=0, i=0;
+		for (i=0; i<pass.length(); i++)
+			seed+=pass.charAt(i)*(i+1);
+		random.setSeed(seed);
+		
+		FileInputStream fr = new FileInputStream(filepath);
+
+		System.out.print("Lendo arquivo... ");
+		int max=fr.available();
+		int num=(int) Math.floor(max/1000000);
+		int resto=max-num*1000000;
+		byte chars[][]=new byte[num][];
+		
+		
+		for (i=0; i<=num; i++){
+			chars[i]=new byte[1000000];	
+			fr.read(chars[i],i*1000000,1000000);
+		}
+		chars[i]=new byte[resto];	
+		fr.read(chars[i],i*1000000,resto);
+		fr.close();
+		
+		System.out.print("completo.\nKriptografando arquivo... ");
+		
+		for (i=0; i<num; i++) {
+			for (int i2=0; i2<1000000; i2++) 
+				chars[i][i2]=(byte) (chars[i][i2]^random.nextInt(256));
+		}
+		for (int i2=0; i2<resto; i2++) 
+			chars[i][i2]=(byte) (chars[i][i2]^random.nextInt(256));
+		
+		System.out.print("completo.\nSalvando arquivo... ");
+		FileOutputStream fw = new FileOutputStream(filepath);
+		
+		for (i=0; i<num; i++) {
+			for (int i2=0; i2<1000000; i2++) 
+				fw.write(chars[i][i2]);
+		}
+		for (int i2=0; i2<resto; i2++) 
+			fw.write(chars[i][i2]);
+		
+		
+		fw.close();
+		System.out.print("completo.");
+	}
+}
