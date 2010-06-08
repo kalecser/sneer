12e72881c786664f08759324fa07db18d68dc93d
igorarouca <igor.arouca@gmail.com>
2010-06-08 15:49:54 -0300

    One-on-one Chat (Fabio's contribution)

---
+++ code/src/sneer/bricks/snapps/contacts/gui/o3chat/OneOnOneChat.java
@@ -0,0 +1,8 @@
+package sneer.bricks.snapps.contacts.gui.o3chat;
+
+import sneer.bricks.software.bricks.snappstarter.Snapp;
+import sneer.foundation.brickness.Brick;
+
+@Snapp
+@Brick
+public interface OneOnOneChat {}
---
+++ code/src/sneer/bricks/snapps/contacts/gui/o3chat/impl/ChatWindow.java
@@ -0,0 +1,11 @@
+package sneer.bricks.snapps.contacts.gui.o3chat.impl;
+
+import javax.swing.JOptionPane;
+
+class ChatWindow {
+
+	static String showInputDialog(String message) {
+		return JOptionPane.showInputDialog(message);
+	}
+
+}
