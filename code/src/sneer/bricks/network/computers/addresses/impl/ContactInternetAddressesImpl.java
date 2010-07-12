4efbd44a5ba27b017ac386f04074486e231ea8cb
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2010-07-12 04:30:25 -0300

    Minor refactoring

--- code/src/sneer/bricks/network/computers/addresses/impl/ContactInternetAddressesImpl.java
+++ code/src/sneer/bricks/network/computers/addresses/impl/ContactInternetAddressesImpl.java
@@ -8,10 +8,7 @@ import sneer.bricks.network.computers.addresses.ContactInternetAddresses;
 import sneer.bricks.network.computers.addresses.keeper.InternetAddress;
 import sneer.bricks.network.computers.addresses.keeper.InternetAddressKeeper;
 import sneer.bricks.network.computers.addresses.sighting.Sighting;
-import sneer.bricks.network.computers.ports.OwnPort;
 import sneer.bricks.network.social.Contact;
-import sneer.bricks.network.social.attributes.Attributes;
-import sneer.bricks.pulp.reactive.Signal;
 import sneer.bricks.pulp.reactive.collections.CollectionChange;
 import sneer.bricks.pulp.reactive.collections.CollectionSignals;
 import sneer.bricks.pulp.reactive.collections.SetRegister;
@@ -22,42 +19,30 @@ class ContactInternetAddressesImpl implements ContactInternetAddresses {
 
 	SetRegister<InternetAddress> _addresses = my(CollectionSignals.class).newSetRegister();
 	
+	
 	@SuppressWarnings("unused")
 	private final WeakContract _refToAvoidGc = my(RemoteTuples.class).addSubscription(Sighting.class, new Consumer<Sighting>() { @Override public void consume(Sighting sighting) {
 		handle(sighting);
 	}});
 
+	
 	@SuppressWarnings("unused")
 	private final WeakContract _refToAvoidGc2 = my(InternetAddressKeeper.class).addresses().addReceiver(new Consumer<CollectionChange<InternetAddress>>() { @Override public void consume(CollectionChange<InternetAddress> change) {
 		_addresses.change(change);
 	}});
+	
 	
 	@Override
 	public SetSignal<InternetAddress> addresses() {
 		return _addresses.output();
 	}
 
+	
 	private void handle(final Sighting sighting) {
 		final Contact contact = my(ContactSeals.class).contactGiven(sighting.peersSeal);
 		if (contact == null) return;
 		
-		_addresses.add(new InternetAddress() {
-			
-			@Override
-			public Signal<Integer> port() {
-				return my(Attributes.class).attributeValueFor(contact, OwnPort.class, Integer.class);
-			}
-			
-			@Override
-			public String host() {
-				return sighting.ip;
-			}
-			
-			@Override
-			public Contact contact() {
-				return contact;
-			}
-		});
+		_addresses.add(new InternetAddressImpl(contact, sighting));
 	}
 
 }
