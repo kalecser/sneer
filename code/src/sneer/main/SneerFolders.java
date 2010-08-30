8b8e28208e2b9fde7a631e941b3e95e51742cbce
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2010-08-30 03:51:33 -0300

    Numbered log files.

--- code/src/sneer/main/SneerFolders.java
+++ code/src/sneer/main/SneerFolders.java
@@ -9,7 +9,7 @@ public class SneerFolders {
 
 	public static final File DATA 			= new File(SNEER_HOME, dummyPrefix() + "data");
 	public static final File TMP 			= new File(SNEER_HOME, dummyPrefix() + "tmp");
-	public static final File LOG_FILE 		= new File(SNEER_HOME, dummyPrefix() + "logs/log.txt");
+	public static final File LOG_FILE 		= new File(SNEER_HOME, dummyPrefix() + "logs/log_" + System.currentTimeMillis() + ".txt");
 	public static final File OWN_CODE 		= new File(CODE, "own");
 	public static final File OWN_BIN 		= new File(CODE, "own/bin");
 
