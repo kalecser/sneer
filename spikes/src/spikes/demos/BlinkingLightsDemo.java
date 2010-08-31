fb7f88faea9dea06dcee0c4f4cf4a5ac7e2436ae
EdmundoAndrade <edmundo@lia.ufc.br>
2010-08-30 22:24:40 -0300

    To easy rendezvous, when the caller is an unknown contact, a persisted blink light is displayed with options: Accept/Reject.

--- spikes/src/spikes/demos/BlinkingLightsDemo.java
+++ spikes/src/spikes/demos/BlinkingLightsDemo.java
@@ -1,17 +1,21 @@
 package spikes.demos;
 
 import static sneer.foundation.environments.Environments.my;
+
+import org.junit.Test;
+
+import sneer.bricks.hardware.gui.actions.Action;
 import sneer.bricks.pulp.blinkinglights.BlinkingLights;
+import sneer.bricks.pulp.blinkinglights.Light;
 import sneer.bricks.pulp.blinkinglights.LightType;
 import sneer.bricks.skin.main.dashboard.Dashboard;
 import sneer.bricks.snapps.system.blinkinglights.gui.BlinkingLightsGui;
-import sneer.foundation.brickness.Brickness;
-import sneer.foundation.environments.Environments;
-import sneer.foundation.lang.ClosureX;
+import sneer.bricks.software.folderconfig.testsupport.BrickTestWithFiles;
 
-public class BlinkingLightsDemo {
+public class BlinkingLightsDemo extends BrickTestWithFiles {
 
-	BlinkingLightsDemo() throws Exception {
+	@Test
+	public void testBL() throws Exception {
 		my(Dashboard.class);
 		my(BlinkingLightsGui.class);
 		BlinkingLights bl = my(BlinkingLights.class);
@@ -19,12 +23,24 @@ public class BlinkingLightsDemo {
 		bl.turnOn(LightType.INFO, "Info", "Info - expires in 7000ms", 7000);
 		bl.turnOn(LightType.WARNING, "Warning", "Warning - expires in 10000ms", 10000);
 		bl.turnOn(LightType.ERROR, "Error", "This is an Error!", new NullPointerException());
+		
+		createInviteFrom("Edmundo");
+		createInviteFrom("Klaus");
+		
+		Thread.sleep(60000); // holds threads for manual UI testing
 	}
 
-	public static void main(String[] args) throws Exception {
-		Environments.runWith(Brickness.newBrickContainer(), new ClosureX<Exception>() { @Override public void run() throws Exception {
-			new BlinkingLightsDemo();
-		}});
+	private void createInviteFrom(String name) {
+		final BlinkingLights bl = my(BlinkingLights.class);
+		final Light light = bl.prepare(LightType.GOOD_NEWS);
+		light.addAction(new Action() {
+			@Override public String caption() {return "Accept";}
+			@Override public void run() {bl.turnOffIfNecessary(light);}
+		});
+		light.addAction(new Action() {
+			@Override public String caption() {return "Reject";}
+			@Override public void run() {bl.turnOffIfNecessary(light);}
+		});
+		bl.turnOnIfNecessary(light, "Invite from " + name, "Click accept to connect.");
 	}
-
 }
