673bb8ad57d9cc76c35e1d76f9d70fa435709c67
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2010-09-06 01:35:34 -0300

    BLs test fixed.

--- code/src/sneer/bricks/pulp/blinkinglights/tests/BlinkingLightsTest.java
+++ code/src/sneer/bricks/pulp/blinkinglights/tests/BlinkingLightsTest.java
@@ -2,18 +2,15 @@ package sneer.bricks.pulp.blinkinglights.tests;
 
 import static sneer.foundation.environments.Environments.my;
 
-import org.jmock.Expectations;
 import org.junit.Test;
 
 import sneer.bricks.hardware.clock.Clock;
-import sneer.bricks.hardware.io.log.Logger;
 import sneer.bricks.pulp.blinkinglights.BlinkingLights;
 import sneer.bricks.pulp.blinkinglights.Light;
 import sneer.bricks.pulp.blinkinglights.LightType;
 import sneer.bricks.pulp.reactive.Signal;
 import sneer.bricks.pulp.reactive.SignalUtils;
 import sneer.bricks.software.folderconfig.testsupport.BrickTestWithFiles;
-import sneer.foundation.brickness.testsupport.Bind;
 
 public class BlinkingLightsTest extends BrickTestWithFiles {
 
@@ -21,14 +18,11 @@ public class BlinkingLightsTest extends BrickTestWithFiles {
 
 	private final Clock _clock = my(Clock.class);
 
-	@Bind final Logger _logger = mock(Logger.class);
-
 	
 	@Test
 	public void testLights() throws Exception {
 		assertLightCount(0, _subject);
 		
-		expectingToLogOneThrowable();
 		Light light = _subject.turnOn(LightType.ERROR, "caption", "some error", new NullPointerException());
 		assertTrue(light.isOn());
 		assertEquals("caption", light.caption());
@@ -58,7 +52,6 @@ public class BlinkingLightsTest extends BrickTestWithFiles {
 		final NullPointerException exception = new NullPointerException();
 		final int timeout = 1000;
 
-		expectingToLogOneThrowable();
 		final Light light = _subject.turnOn(LightType.ERROR, caption, message, exception, timeout);
 		assertTrue(light.isOn());
 
@@ -69,11 +62,6 @@ public class BlinkingLightsTest extends BrickTestWithFiles {
 		my(SignalUtils.class).waitForValue(light.isOn(), false);
 	}
 
-	private void expectingToLogOneThrowable() {
-		checking(new Expectations() {{
-			exactly(1).of(_logger).log(with(any(String.class)), with(any(String.class)));
-		}});
-	}
 
 	private void assertLightCount(int count, BlinkingLights _lights) {
 		assertEquals(count, _lights.lights().size().currentValue().intValue());
