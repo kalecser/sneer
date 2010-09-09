5f9bc79388ca90a32fe954fcc95bf45679c9d729
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2010-09-09 02:29:15 -0300

    Minor doc

--- code/src/sneer/bricks/hardware/io/prevalence/nature/Prevalent.java
+++ code/src/sneer/bricks/hardware/io/prevalence/nature/Prevalent.java
@@ -3,5 +3,12 @@ package sneer.bricks.hardware.io.prevalence.nature;
 import sneer.foundation.brickness.Brick;
 import sneer.foundation.brickness.Nature;
 
+/** Nature that makes bricks transparently prevalent by wrapping them in a prevalent Proxy:
+ *		- void methods are run as Transactions.
+ *		- non-void methods are run as Queries.
+ *		- non-void methods annotated with @Transaction are run as Transactions.
+ *
+ *		IMPORTANT: Prevalent brick instantiation is NOT run as a transaction. It must not have stateful side-effects.
+ * */
 @Brick
 public interface Prevalent extends Nature {}
