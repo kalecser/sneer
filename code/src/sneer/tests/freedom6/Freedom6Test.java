705ffabdc9fbac8db272a2d91311fc7af6dbf341
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2010-09-13 17:27:03 -0300

    Freedom 6 Test started

---
+++ code/src/sneer/tests/freedom6/Freedom6Test.java
@@ -0,0 +1,16 @@
+package sneer.tests.freedom6;
+
+import org.junit.Test;
+
+import sneer.bricks.snapps.wackup.Wackup;
+import sneer.tests.SovereignFunctionalTestBase;
+
+
+public class Freedom6Test extends SovereignFunctionalTestBase {
+
+	@Test (timeout = 3000)
+	public void shareAFile() {
+		//a().loadBrick("Wackup");
+	}
+	
+}
