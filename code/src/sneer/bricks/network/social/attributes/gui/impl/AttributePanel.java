a73901be2c8547fc8c85f7cd39dd6ba32282eb61
igorarouca <igor.arouca@gmail.com>
2010-12-15 15:06:03 -0200

    AttributesWindow: work in progress

--- code/src/sneer/bricks/network/social/attributes/gui/impl/AttributePanel.java
+++ code/src/sneer/bricks/network/social/attributes/gui/impl/AttributePanel.java
@@ -3,18 +3,19 @@ package sneer.bricks.network.social.attributes.gui.impl;
 import javax.swing.JPanel;
 
 import sneer.bricks.network.social.attributes.Attribute;
-import sneer.bricks.network.social.attributes.Attributes;
 
 class AttributePanel extends JPanel {
  
 //	private final TextWidget<JLabel> _name;
 //	private final TextWidget<JTextField> _value;
 
-	AttributePanel(@SuppressWarnings("unused") Attributes controller, @SuppressWarnings("unused") Attribute<?> attribute) {
+	AttributePanel(@SuppressWarnings("unused") Attribute<?> attribute) {
+//		final Attributes attributes = my(Attributes.class);
+
 //		_name = my(ReactiveWidgetFactory.class).newLabel(my(Signals.class).constant(attribute.toString()));
 //		_value = my(ReactiveWidgetFactory.class).newTextField(
-//			controller.myAttributeValue(attribute.getClass()),
-//			controller.myAttributeSetter(attribute.getClass())
+//			attributes.myAttributeValue(attribute.getClass()),
+//			attributes.myAttributeSetter(attribute.getClass())
 //		);
 	}
 
--- code/src/sneer/bricks/network/social/attributes/gui/impl/AttributesWindowImpl.java
+++ code/src/sneer/bricks/network/social/attributes/gui/impl/AttributesWindowImpl.java
@@ -2,25 +2,25 @@ package sneer.bricks.network.social.attributes.gui.impl;
 
 import static sneer.foundation.environments.Environments.my;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
-import sneer.bricks.network.social.attributes.Attributes;
+import sneer.bricks.network.social.attributes.gui.AttributesWindow;
 import sneer.bricks.pulp.reactive.Signal;
 import sneer.bricks.pulp.reactive.Signals;
 import sneer.bricks.skin.main.menu.MainMenu;
 import sneer.bricks.skin.widgets.reactive.ReactiveWidgetFactory;
 import sneer.foundation.lang.Closure;
 
-class AttributesWindowImpl {
-
-	private static final Attributes _controller = my(Attributes.class);
+class AttributesWindowImpl implements AttributesWindow {
 
 	private static final Signal<String> TITLE = my(Signals.class).constant("Attributes Panel");
 
 	private JFrame _frame;
 
     public AttributesWindowImpl() {
-		my(MainMenu.class).addAction(50, "Attributes", new Closure() { @Override synchronized public void run() {
+		my(MainMenu.class).addAction(50, "Preferences", new Closure() { @Override synchronized public void run() {
 			if (_frame == null) _frame = initFrame();
 			_frame.setVisible(true);
 		}});
@@ -29,8 +29,10 @@ class AttributesWindowImpl {
 
 	private JFrame initFrame() {
 		JFrame result = my(ReactiveWidgetFactory.class).newFrame(TITLE).getMainWidget();
-		result.add(new MainPanel(_controller));
+		result.add(new MainPanel());
 		result.setLocationRelativeTo(null);
+		result.setMinimumSize(new Dimension(365, 80));
+	    result.setResizable(false);
 		result.pack();
 
 		return result;
