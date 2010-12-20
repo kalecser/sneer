de4cd89f012ef83ec54e9d4511a462bb3fa27709
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2010-12-20 03:39:17 -0200

    OwnName Attribute is a Snapp to register itself on init.

--- code/src/sneer/bricks/identity/name/OwnName.java
+++ code/src/sneer/bricks/identity/name/OwnName.java
@@ -1,7 +1,11 @@
 package sneer.bricks.identity.name;
 
 import sneer.bricks.network.social.attributes.Attribute;
+import sneer.bricks.software.bricks.snappstarter.Snapp;
+import sneer.foundation.brickness.Brick;
 
+@Brick
+@Snapp
 public interface OwnName extends Attribute<String> {
 
 	String DEFAULT = "";
