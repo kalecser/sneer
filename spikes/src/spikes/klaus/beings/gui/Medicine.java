4023f089577e2471a742708cccf70bfc030525dd
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2010-12-19 22:48:59 -0200

    Playing with Thomas

---
+++ spikes/src/spikes/klaus/beings/gui/Medicine.java
@@ -0,0 +1,21 @@
+package spikes.klaus.beings.gui;
+
+import java.awt.Color;
+
+
+class Medicine extends Germ {
+
+	@Override
+	public Color color() {
+		return Color.BLUE;
+	}
+
+	@Override
+	public void step(Being[][] _world, int x, int y) {
+		if (RANDOM.nextInt(1781) % 3 == 0)
+			super.step(_world, x, y);
+	}
+	
+	
+	
+}
