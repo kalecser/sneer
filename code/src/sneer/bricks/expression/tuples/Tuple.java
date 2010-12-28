3e6757da351eb4179c0520c25a705dc5a693a455
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2010-12-28 01:11:16 -0200

    Comment deleted.

--- code/src/sneer/bricks/expression/tuples/Tuple.java
+++ code/src/sneer/bricks/expression/tuples/Tuple.java
@@ -24,26 +24,4 @@ public abstract class Tuple extends Immutable {
 	
 	public final Seal addressee;
 	
-	
-//	public void stamp(Seal publisher_, long time) {
-//		if (publisher != null)
-//			throw new IllegalStateException("Tuple was already stamped.");
-//
-//		setField("publisher", publisher_);
-////		System.out.println("Setting: " + time);
-//		setField("publicationTime", time);
-////		System.out.println("Set: " + publicationTime);
-//	}
-//
-//
-//	private void setField(String fieldName, Object value) {
-//		try {
-//			Field field = Tuple.class.getField(fieldName);
-//			field.setAccessible(true);
-//			field.set(this, value);
-//		} catch (Exception e) {
-//			throw new IllegalStateException(e);
-//		}
-//	}
-	
 }
