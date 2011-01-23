6ceaa3f55ccf279af93c7ae9cbcd706d92c32881
Rodrigo B. de Oliveira <rbo@acm.org>
2011-01-23 00:55:34 -0800

    unused import removed

--- code/src/sneer/bricks/softwaresharing/stager/tests/BrickStagerTest.java
+++ code/src/sneer/bricks/softwaresharing/stager/tests/BrickStagerTest.java
@@ -8,7 +8,6 @@ import java.util.Collection;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import sneer.bricks.hardware.cpu.lang.contracts.WeakContract;
