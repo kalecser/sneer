77eb51b114553332a381837fcf892bbc580d61cd
Rodrigo B. de Oliveira <rbo@acm.org>
2011-02-04 15:52:58 -0200

    filter Exception out

--- code/src/sneer/bricks/hardware/io/log/filter/impl/LogFilterImpl.java
+++ code/src/sneer/bricks/hardware/io/log/filter/impl/LogFilterImpl.java
@@ -12,7 +12,7 @@ class LogFilterImpl implements LogFilter {
 
 	private final ListRegister<String> _phrases = my(CollectionSignals.class).newListRegister();
 	{
-		_phrases.add("Exception");
+//		_phrases.add("Exception");
 		_phrases.add("thrown");
 		_phrases.add("online");
 		_phrases.add("offline");
