1be014bf5b05877b9b33a147877aa79b3ed80165
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2011-02-11 05:22:59 -0200

    Thread counter logs thread types sorted by count.

--- code/src/sneer/bricks/hardware/cpu/threads/impl/Counter.java
+++ code/src/sneer/bricks/hardware/cpu/threads/impl/Counter.java
@@ -3,6 +3,9 @@ package sneer.bricks.hardware.cpu.threads.impl;
 import static sneer.foundation.environments.Environments.my;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import sneer.bricks.hardware.io.log.Logger;
 import sneer.foundation.lang.CacheMap;
@@ -18,19 +21,23 @@ class Counter {
 	
 	private static void log() {
 		String list = "";
-		for (String key : sortedKeys())
-			list += "\n  " + key + ": " + _countByKey.get(key);
+		for (Entry<String, Integer> entry : entriesSortedByCountThenKey())
+			list += "\n  " + entry.getValue() + ": " + entry.getKey();
 			
 		my(Logger.class).log("Thread count by purpose:", list);
 	}
 
 	
-	private static String[] sortedKeys() {
-		String[] keys = _countByKey.keySet().toArray(new String[0]);
-		Arrays.sort(keys);
-		return keys;
+	private static Map.Entry<String, Integer>[] entriesSortedByCountThenKey() {
+		Entry<String, Integer>[] result = _countByKey.entrySet().toArray(new Map.Entry[0]);
+		Arrays.sort(result, new Comparator<Entry<String, Integer>>() { @Override public int compare(Entry<String, Integer> entry1, Entry<String, Integer> entry2) {
+			int countDif = entry2.getValue().compareTo(entry1.getValue()); //Sort by count, decreasing
+			return countDif == 0 ? entry1.getKey().compareTo(entry2.getKey()): countDif; //Then, sort by key, increasing.
+		}});
+		return result;
 	}
 
+	
 	synchronized
 	static void increment(String key) {
 		incrementByKey(key);
