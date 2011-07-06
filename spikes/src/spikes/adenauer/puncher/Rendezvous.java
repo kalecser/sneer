c5eb74b8048a6a129e499d41f70142187e73cf25
Adenauer Gabriel <adenauer.gabriel@gmail.com>
2011-07-06 13:57:40 -0300

    Print info to console and changed the listener port to 7070.

--- spikes/src/spikes/adenauer/puncher/Rendezvous.java
+++ spikes/src/spikes/adenauer/puncher/Rendezvous.java
@@ -9,17 +9,23 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Rendezvous implements Runnable {
-	private static final String INTERNAL_PORT = "internalPort";
-	private static final String INTERNAL_IP = "internalIP";
-	private static final String EXTERNAL_PORT = "externalPort";
-	private static final String EXTERNAL_IP = "externalIP";
+	private static final String PRIVATE_PORT = "privatePort";
+	private static final String PRIVATE_IP = "privateIP";
+	private static final String PUBLIC_PORT = "publicPort";
+	private static final String PUBLIC_IP = "publicIP";
 
-	private static final Map<String, Map<String, String>> table = new HashMap<String, Map<String,String>>();
+	private static final Map<String, Map<String, String>> _table = new HashMap<String, Map<String,String>>();
+
+
+	public static void main(String[] ignored ) {
+		new Thread(new Rendezvous()).start();
+	}
+	
 	
 	@Override
 	public void run() {
 		try {
-			listener(socket());
+			listener(new DatagramSocket(7070));
 		} catch (Exception e) {
 			display("Failure on listener: " + e.getMessage());
 		}
@@ -35,24 +41,10 @@ public class Rendezvous implements Runnable {
 	
 	private void process(DatagramSocket socket) {
 		try {
-			manager(read(socket), socket);
+			manager(receivedPacketsFrom(socket), socket);
 		} catch (Exception e) {
 			display("Process failure: " + e.getMessage());
 		}
-	}
-
-
-	private byte[] encode(Map<String, String> info) {
-		StringBuilder result = new StringBuilder();
-		result.append(info.get(INTERNAL_IP));
-		result.append(";");
-		result.append(info.get(INTERNAL_PORT));
-		result.append(";");
-		result.append(info.get(EXTERNAL_IP));
-		result.append(";");
-		result.append(info.get(EXTERNAL_PORT));
-		
-		return result.toString().getBytes();
 	}
 
 
@@ -65,32 +57,67 @@ public class Rendezvous implements Runnable {
 	}
 
 	
-	private void forwardInfo(String from, String target, DatagramSocket socket) throws IOException {
-		Map<String, String> fromInfo = table.get(from);
-		Map<String, String> targetInfo = table.get(target);
-		
-		forward(encode(fromInfo), socket, targetInfo.get(EXTERNAL_IP), Integer.valueOf(targetInfo.get(EXTERNAL_PORT)));
-		forward(encode(targetInfo), socket, fromInfo.get(EXTERNAL_IP), Integer.valueOf(fromInfo.get(EXTERNAL_PORT)));
+	private byte[] encode(Map<String, String> info) {
+		StringBuilder result = new StringBuilder();
+		result.append(info.get(PRIVATE_IP));
+		result.append(";");
+		result.append(info.get(PRIVATE_PORT));
+		result.append(";");
+		result.append(info.get(PUBLIC_IP));
+		result.append(";");
+		result.append(info.get(PUBLIC_PORT));
+		return result.toString().getBytes();
 	}
 
+
+	private void forwardInfo(String from, String target, DatagramSocket socket) throws IOException {
+		Map<String, String> fromInfo = _table.get(from);
+		Map<String, String> targetInfo = _table.get(target);
+		
+		if (targetInfo == null || targetInfo.isEmpty()) {
+			display("Info of target: " + target + " not found.");
+			return;
+		}
+		
+		display("Forward info to: " + from);
+		forward(encode(fromInfo), socket, targetInfo.get(PUBLIC_IP), Integer.valueOf(targetInfo.get(PUBLIC_PORT)));
+		display("Forward info to: " + target);
+		forward(encode(targetInfo), socket, fromInfo.get(PUBLIC_IP), Integer.valueOf(fromInfo.get(PUBLIC_PORT)));
+		display("=====================================");
+	}
+
+	
 	private void forward(byte[] data, DatagramSocket socket, String ip, int port) throws IOException {
 		DatagramPacket targetPacket = new DatagramPacket(data, data.length);
 		targetPacket.setSocketAddress(new InetSocketAddress(ip, port));
 		socket.send(targetPacket);
-	}
-
-	private String decodeFromInfo(StringTokenizer fields, DatagramPacket receivedPacket) {
-		String from = fields.nextToken();
-		Map<String, String> data = new HashMap<String, String>();
-		data.put(INTERNAL_IP, fields.nextToken());
-		data.put(INTERNAL_PORT, fields.nextToken());
-		data.put(EXTERNAL_IP, receivedPacket.getAddress().getHostAddress());
-		data.put(EXTERNAL_PORT, String.valueOf(receivedPacket.getPort()));
-		table.put(from, data);
-		return from;
+		display("Sent info: " + new String(data) + " to address: "+ ip + ":" + port);
 	}
 
 	
+	private String decodeFromInfo(StringTokenizer fields, DatagramPacket receivedPacket) {
+		String from = fields.nextToken();
+		Map<String, String> data = new HashMap<String, String>();
+		data.put(PRIVATE_IP, fields.nextToken());
+		data.put(PRIVATE_PORT, fields.nextToken());
+		data.put(PUBLIC_IP, receivedPacket.getAddress().getHostAddress());
+		data.put(PUBLIC_PORT, String.valueOf(receivedPacket.getPort()));
+		
+		refreshTable(from, data);
+		
+		display("Decoded packet id: " + from + " - Private address: " + data.get(PRIVATE_IP) + ", Private port: " + data.get(PRIVATE_PORT) +   ", Public address: " + data.get(PUBLIC_IP) + " and Public port: "+  data.get(PUBLIC_PORT));
+		return from;
+	}
+
+
+	private void refreshTable(String from, Map<String, String> data) {
+		if (from == null) return;
+		if (data == null || data.isEmpty()) return;
+		if (_table.containsKey(from)) _table.remove(from);
+		_table.put(from, data);
+	}
+
+
 	private String decodeTargetName(StringTokenizer fields) {
 		return fields.nextToken();
 	}
@@ -102,24 +129,16 @@ public class Rendezvous implements Runnable {
 	}
 
 
-	private DatagramPacket read(DatagramSocket socket) throws IOException {
+	private DatagramPacket receivedPacketsFrom(DatagramSocket socket) throws IOException {
 		byte[] receivedData = new byte[1024];
 		DatagramPacket receivedPacket = new DatagramPacket(receivedData, receivedData.length);
-		
 		socket.receive(receivedPacket);
 		return receivedPacket;
 	}
 
 	
-	private DatagramSocket socket() throws IOException {
-		return new DatagramSocket(9876);
-	}
-	
 	private void display(String out) {
 		System.out.println(out);
 	}
 
-	public static void main(String[] ignored ) {
-		new Thread(new Rendezvous()).start();
-	}
 }
