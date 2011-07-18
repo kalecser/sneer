e510a9f2772e466f880bc984545bb5e1e9d0fce3
unknown <Junior@.(none)>
2011-07-18 18:43:39 -0300

    Draft UDP test to new network

--- spikes/src/spikes/adenauer/network/tests/NetworkTest.java
+++ spikes/src/spikes/adenauer/network/tests/NetworkTest.java
@@ -13,35 +13,65 @@ import sneer.bricks.hardware.cpu.threads.Threads;
 import sneer.bricks.identity.seals.Seal;
 import sneer.bricks.pulp.events.EventSource;
 import sneer.bricks.software.folderconfig.testsupport.BrickTestBase;
-import sneer.foundation.environments.Environments;
 import sneer.foundation.lang.Closure;
 import sneer.foundation.lang.Consumer;
 import sneer.foundation.lang.arrays.ImmutableByteArray;
 import spikes.adenauer.network.CompositeNetwork;
 import spikes.adenauer.network.Network.Packet;
+import spikes.adenauer.network.UdpNetwork;
 
 
 public class NetworkTest extends BrickTestBase {
 	private final Threads _threads = my(Threads.class);
-
-	@Test	
-	public void udpNetworkTest() throws Exception {
-		_threads.startDaemon("Loopback peer", new LoopbackPeer());
+	private final CompositeNetwork _peerNetwork = my(CompositeNetwork.class);
+	
+	@SuppressWarnings("unused")	private final WeakContract _peerRefToAvoidGC;
+	
+	public NetworkTest() {
+		_peerRefToAvoidGC = peerPacketReceiver().addReceiver(new Consumer<Packet>() { @Override public void consume(Packet packet) {
+			String data = new String(packet.data());
+			assertEquals(data, "HELLO");
+		}});
 	}
 	
+	
+	@Test	
+	public void udpNetworkTest() throws Exception {	
+		_threads.startDaemon("Loopback peer", new LoopbackPeer());
+		_peerNetwork.add(my(UdpNetwork.class));
+		_peerNetwork.send("hello".getBytes(), dummySeal()); 
+	}
+	
+	
+	private EventSource<Packet> peerPacketReceiver() {
+		return _peerNetwork.packetsReceived();
+	}
+	
+	
+	private Seal dummySeal() {
+		try {
+			return new Seal(new ImmutableByteArray(my(Codec.class).hex().decode("c0e0ae71b239640fded22b880f7cb63772f04a6f9c7685689c2610f395dbff1d3cfc11f36f20d54305ff51b26cd171e4882d628ea4a1ac201641cf17fea6912c")));
+		} catch (DecodeException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	
 	final class LoopbackPeer implements Closure {
-		private final CompositeNetwork _network = Environments.my(CompositeNetwork.class);
+		private final CompositeNetwork _network = my(CompositeNetwork.class);
 		
-		@SuppressWarnings("unused")	private WeakContract _regToAvoidGC;
+		@SuppressWarnings("unused")	private WeakContract _refToAvoidGC;
 		
 		public LoopbackPeer() {
-			_regToAvoidGC = networkReceiver().addReceiver(new Consumer<Packet>(){ @Override public void consume(Packet packet) {
+			_refToAvoidGC = networkReceiver().addReceiver(new Consumer<Packet>(){ @Override public void consume(Packet packet) {
 				loopback(packet);
 			}});
 		} 
 
 		@Override
 		public void run() {
+			_network.add(my(UdpNetwork.class));
+			
 		}
 
 		private void loopback(Packet packet) {
@@ -57,14 +87,6 @@ public class NetworkTest extends BrickTestBase {
 		private EventSource<Packet> networkReceiver() {
 			return _network.packetsReceived();
 		}
-		
-		
-		private Seal dummySeal() {
-			try {
-				return new Seal(new ImmutableByteArray(my(Codec.class).hex().decode("c0e0ae71b239640fded22b880f7cb63772f04a6f9c7685689c2610f395dbff1d3cfc11f36f20d54305ff51b26cd171e4882d628ea4a1ac201641cf17fea6912c")));
-			} catch (DecodeException e) {
-				throw new IllegalStateException(e);
-			}
-		}
-	}	
+	}
+	
 }
