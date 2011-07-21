335f1352b6abc3d5f8bf1217873cbde54f81ede6
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2011-07-21 19:03:29 -0300

    New netwrok tuple proposal

--- spikes/src/spikes/adenauer/network/Network.java
+++ spikes/src/spikes/adenauer/network/Network.java
@@ -1,3 +1,17 @@
+// Heartbeat (tuple) "Im alive at this IP and port" flooded every 15 seconds
+//		OwnName
+//		OwnLocalIp
+//		OwnPort
+//
+// PeerLookup (tuple) "Where is this peer?" flooded every 15 seconds for offline peers that are issueing heartbeats
+//		PeerSeal
+//
+// PeerSighting (tuple) "I have seen this peer on this IP and Port" sent to whoever did a lookup.
+//		Seal (peer)
+//		IP, port
+//
+
+
 package spikes.adenauer.network;
 
 import sneer.bricks.identity.seals.Seal;
