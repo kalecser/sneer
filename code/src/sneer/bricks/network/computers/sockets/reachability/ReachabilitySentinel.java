d35de8a7f078dcf77b0a58d7af45f0f5a6c4860c
Adenauer Gabriel <adenauer.gabriel@gmail.com>
2011-07-26 15:23:13 -0300

    unreacheable BL removed.

--- code/src/sneer/bricks/network/computers/sockets/reachability/ReachabilitySentinel.java
+++
@@ -1,8 +0,0 @@
-package sneer.bricks.network.computers.sockets.reachability;
-
-import sneer.bricks.software.bricks.snapploader.Snapp;
-import sneer.foundation.brickness.Brick;
-
-@Snapp
-@Brick
-public interface ReachabilitySentinel {}
