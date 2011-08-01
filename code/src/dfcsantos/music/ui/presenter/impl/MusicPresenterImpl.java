1cded0295afd2ffa930bcd804481d2a3d8d8b5bd
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2011-08-01 18:12:37 -0300

    TrackFolder choice working.

--- code/src/dfcsantos/music/ui/presenter/impl/MusicPresenterImpl.java
+++ code/src/dfcsantos/music/ui/presenter/impl/MusicPresenterImpl.java
@@ -1,35 +1,52 @@
 package dfcsantos.music.ui.presenter.impl;
 
 import static sneer.foundation.environments.Environments.my;
-import sneer.bricks.pulp.reactive.Register;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
 import sneer.bricks.pulp.reactive.Signal;
-import sneer.bricks.pulp.reactive.Signals;
 import sneer.bricks.skin.main.instrumentregistry.InstrumentRegistry;
+import sneer.bricks.skin.notmodal.filechooser.FileChoosers;
+import sneer.foundation.lang.Consumer;
+import dfcsantos.music.Music;
 import dfcsantos.music.ui.presenter.MusicPresenter;
 import dfcsantos.music.ui.view.MusicView;
 import dfcsantos.music.ui.view.MusicViewListener;
 
 class MusicPresenterImpl implements MusicPresenter, MusicViewListener {
 
-	private Register<Boolean> isExchangingTracks = my(Signals.class).newRegister(false);
+	private final JFileChooser tracksFolderChooser;
 
 	{
-		my(InstrumentRegistry.class).registerInstrument(my(MusicView.class).initInstrument(this));
+        tracksFolderChooser = initTracksFolderChooser();
+    	my(InstrumentRegistry.class).registerInstrument(my(MusicView.class).initInstrument(this));
+	}
+
+	
+	private JFileChooser initTracksFolderChooser() {
+		JFileChooser chooser = my(FileChoosers.class).newFileChooser(new Consumer<File>() { @Override public void consume(File chosenFolder) {
+        	if (chosenFolder != null)
+        		my(Music.class).setSharedTracksFolder(chosenFolder);
+    	}}, JFileChooser.DIRECTORIES_ONLY);
+		chooser.setCurrentDirectory(my(Music.class).sharedTracksFolder().currentValue());
+		return chooser;
 	}
 
 	@Override
 	public void chooseTracksFolder() {
-		throw new sneer.foundation.lang.exceptions.NotImplementedYet(); // Implement
+    	tracksFolderChooser.showOpenDialog(null);
 	}
 
 	@Override
 	public Signal<Boolean> isExchangingTracks() {
-		return isExchangingTracks.output();
+		return my(Music.class).isTrackExchangeActive();
 	}
 
 	@Override
 	public void toggleTrackExchange() {
-		isExchangingTracks.setter().consume(!isExchangingTracks().currentValue());
+		my(Music.class).trackExchangeActivator().consume(!isExchangingTracks().currentValue());
 	}
 
 }
