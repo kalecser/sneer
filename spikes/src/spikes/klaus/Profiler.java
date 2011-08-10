ea43eae8a1e0e8281ee2507c6be07a82c5dbddc5
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2011-08-10 16:12:38 -0300

    Minor spikes.

---
+++ spikes/src/spikes/klaus/Profiler.java
@@ -0,0 +1,11 @@
+package spikes.klaus;
+
+
+public class Profiler {
+
+	public static void main(String[] args) {
+		//Map<Thread, StackTraceElement[]> allStackTraces = Thread.getAllStackTraces();
+		//Thread.currentThread().getState().
+	}
+
+}
