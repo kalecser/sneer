f83e15c71894d6537128e8023899aea120961fba
Adenauer Gabriel <adenauer.gabriel@gmail.com>
2011-08-12 15:07:00 -0300

    Call Music Instrument on the Main Menu.

---
+++ code/src/dfcsantos/music/menuitem/MusicMenuItem.java
@@ -0,0 +1,9 @@
+package dfcsantos.music.menuitem;
+
+import sneer.bricks.hardware.gui.nature.GUI;
+import sneer.bricks.software.bricks.snapploader.Snapp;
+import sneer.foundation.brickness.Brick;
+
+@Snapp
+@Brick(GUI.class)
+public interface MusicMenuItem {}
--- code/src/dfcsantos/music/ui/presenter/MusicPresenter.java
+++ code/src/dfcsantos/music/ui/presenter/MusicPresenter.java
@@ -1,8 +1,6 @@
 package dfcsantos.music.ui.presenter;
 
-import sneer.bricks.software.bricks.snapploader.Snapp;
 import sneer.foundation.brickness.Brick;
 
 @Brick
-@Snapp
 public interface MusicPresenter {}
