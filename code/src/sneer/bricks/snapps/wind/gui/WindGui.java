1feb13477798f103865996e0e4065f5c686763e6
Adenauer Gabriel <adenauer.gabriel@gmail.com>
2011-08-12 13:35:36 -0300

    Removed Wind from Dashboard and blank Wind (PrivateChat).

--- code/src/sneer/bricks/snapps/wind/gui/WindGui.java
+++ code/src/sneer/bricks/snapps/wind/gui/WindGui.java
@@ -1,9 +1,8 @@
 package sneer.bricks.snapps.wind.gui;
 
 import sneer.bricks.skin.main.instrumentregistry.Instrument;
-import sneer.bricks.software.bricks.snapploader.Snapp;
 import sneer.foundation.brickness.Brick;
 
-@Snapp
+//@Snapp Just commented to hide on Dashboard. It will return later as the new Chat.  
 @Brick
 public interface WindGui extends Instrument {}
