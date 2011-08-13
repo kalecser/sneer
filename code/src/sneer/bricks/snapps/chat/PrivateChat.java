17f7f151efae93ad9c564ec89e2e1cd04d4531d0
Adenauer Gabriel <adenauer.gabriel@gmail.com>
2011-08-13 10:58:07 -0300

    Removed Wind (blank) call from private chat to activate it again.

--- code/src/sneer/bricks/snapps/chat/PrivateChat.java
+++ code/src/sneer/bricks/snapps/chat/PrivateChat.java
@@ -1,7 +1,8 @@
 package sneer.bricks.snapps.chat;
 
+import sneer.bricks.software.bricks.snapploader.Snapp;
 import sneer.foundation.brickness.Brick;
 
-//@Snapp  Just commented to hide on Dashboard. It will return later as the new Chat.
+@Snapp //  Just commented to hide on Dashboard. It will return later as the new Chat.
 @Brick
 public interface PrivateChat {}
