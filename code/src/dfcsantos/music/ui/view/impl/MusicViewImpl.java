035bea1bbb1b2835f6cdf4510083628575fd2229
Adenauer Gabriel <adenauer.gabriel@gmail.com>
2011-09-05 14:42:50 -0300

    Extracted to SmileyPanel.

--- code/src/dfcsantos/music/ui/view/impl/MusicViewImpl.java
+++ code/src/dfcsantos/music/ui/view/impl/MusicViewImpl.java
@@ -1,14 +1,9 @@
 package dfcsantos.music.ui.view.impl;
 
 import java.awt.Container;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JComponent;
-import javax.swing.JPanel;
 
 import sneer.bricks.skin.main.dashboard.InstrumentPanel;
 import sneer.bricks.skin.menu.MenuGroup;
@@ -18,16 +13,14 @@ import dfcsantos.music.ui.view.MusicViewListener;
 class MusicViewImpl implements MusicView {
 	private MusicViewListener listener;
 
-	@SuppressWarnings("unused") private Object refToAvoidGc, trackNameRefToAvoidGc, trackTimeRefToAvoidGc, subFordersRefToAvoidGc;
-
 	@Override
 	public void init(InstrumentPanel container) {
 		Container pane = container.contentPane();
 		pane.setLayout(new GridLayout(4, 1));
-		pane.add(new SelectorModePanel(listener));
-		pane.add(new DisplayerPanel(listener));
-		pane.add(new PlayerPanel(listener));
-		pane.add(emotions());
+		pane.add(new FolderSelectionPanel(listener));
+		pane.add(new PlayingTrackPanel(listener));
+		pane.add(new PlayerControlsPanel(listener));
+		pane.add(new SmileyPanel(listener));
 		
 		initMenu(container.actions());
 	}
@@ -45,42 +38,6 @@ class MusicViewImpl implements MusicView {
 		}});
 	}
 
-	
-	private JPanel emotions() {
-		JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 10, 0));
-		panel.add(meTooButton());
-		panel.add(deleteButton());
-		panel.add(noWayButton());
-		return panel;
-	}
-
-
-	private JButton meTooButton() {
-		JButton meToo = new JButton(":D");
-		meToo.addActionListener(new ActionListener() {  @Override public void actionPerformed(ActionEvent e) {
-			listener.meToo();
-		}});
-		return meToo;
-	}
-
-
-	private JButton deleteButton() {
-		JButton delete = new JButton(":P");
-		delete.addActionListener(new ActionListener() {  @Override public void actionPerformed(ActionEvent e) {
-			listener.deleteTrack();
-		}});
-		return delete;
-	}
-
-
-	private JButton noWayButton() {
-		JButton noWay = new JButton(":(");
-		noWay.addActionListener(new ActionListener() {  @Override public void actionPerformed(ActionEvent e) {
-			listener.noWay();
-		}});
-		return noWay;
-	}
-	
 	
 	@Override
 	public int defaultHeight() {
