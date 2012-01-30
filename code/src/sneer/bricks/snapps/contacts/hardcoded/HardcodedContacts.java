d7787643661539330bd7b923a12e5091e03e9ea1
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2012-01-30 19:29:18 -0200

    HardcodedContacts are no more.

--- code/src/sneer/bricks/snapps/contacts/hardcoded/HardcodedContacts.java
+++
@@ -1,8 +0,0 @@
-package sneer.bricks.snapps.contacts.hardcoded;
-
-import sneer.bricks.software.bricks.snapploader.Snapp;
-import sneer.foundation.brickness.Brick;
-
-@Snapp
-@Brick
-public interface HardcodedContacts {}
