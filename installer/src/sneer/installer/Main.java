396a5b1d1840026208cdf5ab04cee9e480315d0d
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2012-02-03 01:12:15 -0200

    Java1.6 required.

--- installer/src/sneer/installer/Main.java
+++ installer/src/sneer/installer/Main.java
@@ -1,11 +1,16 @@
 package sneer.installer;
 
 import static sneer.main.SneerCodeFolders.SNEER_HOME;
+
+import javax.swing.JOptionPane;
+
 import sneer.main.Sneer;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		checkJava1_6();
+		
 		if(SNEER_HOME.exists())
 			new Installation();
 		else
@@ -16,6 +21,14 @@ public class Main {
 	}
 
 	
+	private static void checkJava1_6() {
+		String version = "" + System.getProperty("java.version");
+		if (version.startsWith("1.6")) return;
+		JOptionPane.showMessageDialog(null, "Sneer requires Java 6 to run. You are using Java " + version);
+		System.exit(0);
+	}
+
+
 	private static void overcomeWebstartSecurityRestrictions() {
 		System.setSecurityManager(new PermissiveSecurityManager());
 	}
