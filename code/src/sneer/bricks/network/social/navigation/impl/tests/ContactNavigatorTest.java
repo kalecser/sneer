a8cbe95cbb53861ca56f6a3471731c7355ccc03d
kalecser <kalecser@gmail.com>
2012-03-28 10:12:27 -0300

    Contacts Navigation test

--- code/src/sneer/bricks/network/social/navigation/impl/tests/ContactNavigatorTest.java
+++ code/src/sneer/bricks/network/social/navigation/impl/tests/ContactNavigatorTest.java
@@ -4,17 +4,14 @@ import static basis.environments.Environments.my;
 
 import org.junit.Test;
 
-import sneer.bricks.expression.tuples.TupleSpace;
 import sneer.bricks.expression.tuples.testsupport.BrickTestWithTuples;
-import sneer.bricks.hardware.cpu.lang.contracts.WeakContract;
 import sneer.bricks.identity.seals.OwnSeal;
 import sneer.bricks.identity.seals.Seal;
 import sneer.bricks.network.social.Contacts;
 import sneer.bricks.network.social.navigation.ContactNavigator;
 import sneer.bricks.network.social.navigation.ContactOfContact;
-import sneer.bricks.network.social.navigation.ContactsRequest;
 import basis.environments.Environments;
-import basis.lang.Closure;
+import basis.lang.ClosureX;
 import basis.lang.Consumer;
 import basis.lang.exceptions.Refusal;
 import basis.util.concurrent.Latch;
@@ -22,52 +19,29 @@ import basis.util.concurrent.Latch;
 public class ContactNavigatorTest extends BrickTestWithTuples {
 	
 	private final ContactNavigator subject = my(ContactNavigator.class);
-	protected Seal seal;
+	protected Seal remoteSeal;
+	
 	
 	@Test(timeout=4000)
-	public void searchReply(){
-		final Seal ownSeal = ownSeal();
-		
-		Environments.runWith(remote(), new Closure(){  @Override public void run() {
-			seal = ownSeal();			
+	public void requestContacts() throws Refusal{
+		Environments.runWith(remote(), new ClosureX<Refusal>(){  @Override public void run() throws Refusal {
+			remoteSeal = ownSeal();			
+			my(Contacts.class).addContact("Neide");
+			my(ContactNavigator.class);
 		}});
 		
 		final Latch latch = new Latch();
 				
-		subject.searchContactsOf(seal, new Consumer<ContactOfContact>(){  @Override public void consume(ContactOfContact value) {
-			latch.open();
-		}});
-					
-		Environments.runWith(remote(), new Closure(){  @Override public void run() {
-			my(TupleSpace.class).add(new ContactOfContact("Fred", new Seal(new byte[]{1}), ownSeal));
-		}});
-		
-		latch.waitTillOpen();
-	}
-	
-	@Test(timeout=4000)
-	public void searchRequest() throws Refusal{				
-		final Latch latch = new Latch();
-		
-		@SuppressWarnings("unused")
-		WeakContract ref = my(TupleSpace.class).addSubscription(ContactOfContact.class, new Consumer<ContactOfContact>(){  @Override public void consume(ContactOfContact value) {
+		subject.searchContactsOf(remoteSeal, new Consumer<ContactOfContact>(){  @Override public void consume(ContactOfContact value) {
 			if (value.nick.equals("Neide"))
 				latch.open();
 		}});
+					
 		
-		my(Contacts.class).addContact("Neide");
-		receiveContactsRequest();
-				
 		latch.waitTillOpen();
 	}
 
-	private void receiveContactsRequest() {
-		final Seal ownSeal = ownSeal();
-		Environments.runWith(remote(), new Closure(){  @Override public void run() {
-			my(TupleSpace.class).add(new ContactsRequest(ownSeal));				
-		}});
-	}
-
+	
 	private Seal ownSeal() {
 		return my(OwnSeal.class).get().currentValue();
 	}
