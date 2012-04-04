6b74918349a60226c7c30bff24acb2b23c2ab3a3
Klaus Wuestefeld <klauswuestefeld@gmail.com>
2012-04-04 18:47:09 -0300

    ContactNavigator must be a Snapp to respond to requests from other machines.

--- code/src/sneer/bricks/network/social/navigation/ContactNavigator.java
+++ code/src/sneer/bricks/network/social/navigation/ContactNavigator.java
@@ -1,9 +1,11 @@
 package sneer.bricks.network.social.navigation;
 
 import sneer.bricks.identity.seals.Seal;
+import sneer.bricks.software.bricks.snapploader.Snapp;
 import basis.brickness.Brick;
 import basis.lang.Consumer;
 
+@Snapp
 @Brick
 public interface ContactNavigator {
 
