919b0607dde3ae9fb2891c8adac8e74a1f9c90c0
Adenauer Gabriel <adenauer.gabriel@gmail.com>
2012-05-06 19:18:58 +0200

    UPnP timer.

--- code/src/sneer/bricks/network/computers/upnp/impl/UpnpImpl.java
+++ code/src/sneer/bricks/network/computers/upnp/impl/UpnpImpl.java
@@ -8,6 +8,7 @@ import java.net.UnknownHostException;
 
 import net.sbbi.upnp.impls.InternetGatewayDevice;
 import net.sbbi.upnp.messages.UPNPResponseException;
+import sneer.bricks.hardware.clock.timer.Timer;
 import sneer.bricks.hardware.cpu.threads.Threads;
 import sneer.bricks.hardware.io.log.Logger;
 import sneer.bricks.network.computers.ports.OwnPort;
@@ -25,14 +26,39 @@ public class UpnpImpl implements Upnp {
 	
 	private static final String TCP_PROTOCOL = "TCP";
 	private static final String UDP_PROTOCOL = "UDP";
-	@SuppressWarnings("unused") private final Object _refToAvoidGc;
+	private static final int FIVE_MINUTES = 1000 * 60 * 5;
+	private String _previousMappedIp = null;
 	private int _previousMappedPort = 0;
+	@SuppressWarnings("unused") private final Object _refToAvoidGc1, refToAvoidGc2;
 
-	
+
 	private UpnpImpl() {
-		_refToAvoidGc = ownPort().addReceiver(new Consumer<Integer>() { @Override public void consume(Integer port) {
+		_refToAvoidGc1 = ownPort().addReceiver(new Consumer<Integer>() { @Override public void consume(Integer port) {
 			startMapping(port);
 		}});
+
+		refToAvoidGc2 = my(Timer.class).wakeUpEvery(FIVE_MINUTES, new Runnable() { @Override public void run() {
+			changeLocalHostIp();
+		}});
+	}
+
+	
+	synchronized
+	private void changeLocalHostIp() {
+		
+		try {
+			changeLocalHostIpIfNecessary();
+		} catch (Exception e) {
+			blink(e, "local host ip " + _previousMappedIp);
+		}
+	}
+
+	
+	private void changeLocalHostIpIfNecessary() throws Exception {
+		String currentIp = localHostIp();
+		if (currentIp.equals(_previousMappedIp)) return; 
+
+		tryToUpdateMap(currentIp, _previousMappedPort);
 	}
 
 	
@@ -46,16 +72,15 @@ public class UpnpImpl implements Upnp {
 	synchronized
 	private void map(int port) {
 		try {
-			tryToUpdateMap(port);
+			tryToUpdateMap(localHostIp(), port);
 		} catch (Exception e) {
 			blink(e, "port " + port);
-		} finally {
-			_previousMappedPort = port;
 		}
 	}
 
 
-	private void tryToUpdateMap(int port) throws Exception {  //sbbi lib throws RuntimeExceptions :(
+	synchronized
+	private void tryToUpdateMap(String ip, int port) throws Exception {  //sbbi lib throws RuntimeExceptions :(
 		InternetGatewayDevice[] devices = InternetGatewayDevice.getDevices(5000);
 		if (devices == null || devices.length == 0) {
 			my(BlinkingLights.class).turnOn(LightType.INFO, "No UPnP devices found.", "There are apparently no UPnP devices on your network. That's OK.", 6000);
@@ -63,23 +88,25 @@ public class UpnpImpl implements Upnp {
 		}
 		
 		for (InternetGatewayDevice device : devices)
-			updateMap(device, port);
+			updateMap(device, ip, port);
 	}
 	
 	
-	private void updateMap(InternetGatewayDevice device, int port) {
+	private void updateMap(InternetGatewayDevice device, String ip, int port) {
 		try {
-			tryToUpdateMapping(device, port);
+			tryToUpdateMapping(device, ip, port);
 		} catch (Exception e) {
 			blink(e, "port " + port + " on device " + pretty(device));
 		}
 	}
 
 
-	private void tryToUpdateMapping(InternetGatewayDevice device, int port) throws Exception { //sbbi lib throws RuntimeExceptions :(
-		String ip = localHostIp();
+	private void tryToUpdateMapping(InternetGatewayDevice device, String ip, int port) throws Exception { //sbbi lib throws RuntimeExceptions :(
 		addMappingIfNecessary(device, ip, port);
 		deleteMappingIfNecessary(device, _previousMappedPort);
+		
+		_previousMappedIp = ip;
+		_previousMappedPort = port;
 	}
 
 
@@ -97,8 +124,15 @@ public class UpnpImpl implements Upnp {
 	}
 	
 
-	private void deleteMappingIfNecessary(InternetGatewayDevice device, int previousPort) {
+	private void deleteMappingIfNecessary(InternetGatewayDevice device, int previousPort) throws Exception {
 		if (previousPort == 0) return;
+		if (!localHostIp().equals(_previousMappedIp)) return; //if local host ip changed not remove the mapping.
+		
+		tryToDeleteMapping(device, previousPort);
+	}
+
+	
+	private void tryToDeleteMapping(InternetGatewayDevice device, int previousPort) {
 		try {
 			deleteMapping(device, previousPort, UDP_PROTOCOL);
 			deleteMapping(device, previousPort, TCP_PROTOCOL);
@@ -106,7 +140,7 @@ public class UpnpImpl implements Upnp {
 			my(Logger.class).log("UPnP failed to delete old port " + previousPort + ". " + e.getClass() + ": " + e.getMessage());
 		}
 	}
-
+	
 	
 	private void deleteMapping(InternetGatewayDevice device, int previousPort, String protocol) throws IOException, UPNPResponseException {
 		if (device.deletePortMapping(null, previousPort, protocol))
