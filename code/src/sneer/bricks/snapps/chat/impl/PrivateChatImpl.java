640d662f5f65c350abd5bfbdda64ff82f63fe5b3
alexmreis <alex@alexmreis.com>
2012-05-08 19:55:35 +0200

    Refactored chat snapp

--- code/src/sneer/bricks/snapps/chat/impl/PrivateChatImpl.java
+++ code/src/sneer/bricks/snapps/chat/impl/PrivateChatImpl.java
@@ -1,12 +1,13 @@
 package sneer.bricks.snapps.chat.impl;
 
 import static basis.environments.Environments.my;
-import basis.lang.Consumer;
+
+import javax.swing.JOptionPane;
+
 import sneer.bricks.expression.tuples.TupleSpace;
 import sneer.bricks.expression.tuples.remote.RemoteTuples;
 import sneer.bricks.hardware.clock.Clock;
 import sneer.bricks.hardware.cpu.lang.contracts.WeakContract;
-import sneer.bricks.identity.seals.OwnSeal;
 import sneer.bricks.identity.seals.Seal;
 import sneer.bricks.identity.seals.contacts.ContactSeals;
 import sneer.bricks.network.social.Contact;
@@ -15,6 +16,7 @@ import sneer.bricks.snapps.chat.PrivateChat;
 import sneer.bricks.snapps.contacts.actions.ContactAction;
 import sneer.bricks.snapps.contacts.actions.ContactActionManager;
 import sneer.bricks.snapps.contacts.gui.ContactsGui;
+import basis.lang.Consumer;
 
 class PrivateChatImpl implements PrivateChat {
 
@@ -22,13 +24,11 @@ class PrivateChatImpl implements PrivateChat {
 	@SuppressWarnings("unused") private final WeakContract _refToAvoidGc;
 
 	{
-		//initFrame();
-		
 		_refToAvoidGc = my(RemoteTuples.class).addSubscription(ChatMessage.class, new Consumer<ChatMessage>() { @Override public void consume(ChatMessage message) {
-			if (!isForMe(message)) return;
+			if (isPublic(message)) return;
 			if (isOld(message)) return;
-			
-			String reply = ChatWindow.showInputDialog(my(ContactSeals.class).contactGiven(message.publisher).nickname() + " says: " + message.text);
+			String messagePrompt = my(ContactSeals.class).contactGiven(message.publisher).nickname() + " says: " + message.text;
+			String reply = showInputDialog(messagePrompt);
 			if (reply == null) return;
 			sendTo(message.publisher, reply);
 		}});
@@ -37,7 +37,7 @@ class PrivateChatImpl implements PrivateChat {
 			@Override
 			public void run() {
 				Contact contact = my(ContactsGui.class).selectedContact().currentValue();
-				String message = ChatWindow.showInputDialog("Say something to " + contact.nickname());
+				String message = showInputDialog("Say something to " + contact.nickname());
 				if (message == null || message.trim().isEmpty()) return;
 				Seal to = my(ContactSeals.class).sealGiven(contact).currentValue();
 				sendTo(to, message);
@@ -51,25 +51,13 @@ class PrivateChatImpl implements PrivateChat {
 		});
 	}
 
-
-	//private void initFrame() {
-	//	my(GuiThread.class).invokeLater(new Closure() { @Override public void run() {
-	//		JFrame chatFrame = new JFrame();
-	//		chatFrame.add(my(ChatPanels.class).newPanel(my(Wind.class).shoutsHeard(), my(Wind.class).megaphone()));
-	//		chatFrame.setBounds(100, 100, 300, 500);
-	//		chatFrame.setVisible(true);
-	//	}});		
-	//}
-
-	
 	private void sendTo(Seal to, String text) {
 		my(TupleSpace.class).add(new ChatMessage(to, text));
 	}
 	
 	
-	private boolean isForMe(ChatMessage message) {
-		Seal ownSeal = my(OwnSeal.class).get().currentValue();
-		return ownSeal.equals(message.addressee);
+	private boolean isPublic(ChatMessage message) {		
+		return message.addressee == null;
 	}
 
 	
@@ -80,6 +68,10 @@ class PrivateChatImpl implements PrivateChat {
 	
 	private long now() {
 		return my(Clock.class).time().currentValue();
+	}
+	
+	private String showInputDialog(String messagePrompt) {
+		return JOptionPane.showInputDialog(messagePrompt);
 	}
 
 }
