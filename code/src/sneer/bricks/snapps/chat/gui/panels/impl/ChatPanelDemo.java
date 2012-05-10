8f558edc31ed64fd3f519ad1434877b8194e4bd9
kalecser <kalecser@gmail.com>
2012-05-10 08:53:31 -0300

    Removing warnings

--- code/src/sneer/bricks/snapps/chat/gui/panels/impl/ChatPanelDemo.java
+++ code/src/sneer/bricks/snapps/chat/gui/panels/impl/ChatPanelDemo.java
@@ -3,36 +3,21 @@ package sneer.bricks.snapps.chat.gui.panels.impl;
 import static basis.environments.Environments.my;
 
 import java.awt.BorderLayout;
-import java.awt.Container;
-import java.util.ListResourceBundle;
 
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 
+import sneer.bricks.hardware.gui.timebox.TimeboxedEventQueue;
+import sneer.bricks.pulp.reactive.collections.CollectionSignals;
+import sneer.bricks.pulp.reactive.collections.ListRegister;
+import sneer.bricks.snapps.chat.ChatMessage;
+import sneer.bricks.snapps.chat.gui.panels.ChatPanels;
 import basis.brickness.Brickness;
 import basis.environments.Environments;
 import basis.lang.Closure;
 import basis.lang.Consumer;
-
-import sneer.bricks.hardware.clock.timer.Timer;
-import sneer.bricks.hardware.cpu.lang.contracts.WeakContract;
-import sneer.bricks.hardware.gui.timebox.TimeboxedEventQueue;
-import sneer.bricks.pulp.reactive.Register;
-import sneer.bricks.pulp.reactive.Signal;
-import sneer.bricks.pulp.reactive.Signals;
-import sneer.bricks.pulp.reactive.collections.CollectionSignals;
-import sneer.bricks.pulp.reactive.collections.ListRegister;
-import sneer.bricks.pulp.reactive.collections.ListSignal;
-import sneer.bricks.skin.main.dashboard.InstrumentPanel;
-import sneer.bricks.skin.main.menu.MainMenu;
-import sneer.bricks.skin.menu.MenuGroup;
-import sneer.bricks.snapps.chat.ChatMessage;
-import sneer.bricks.snapps.chat.gui.panels.ChatPanels;
-import dfcsantos.music.ui.view.MusicView;
-import dfcsantos.music.ui.view.MusicViewListener;
 
 
 class ChatPanelDemo {
@@ -69,7 +54,7 @@ class ChatPanelDemo {
 		};
 		final JPanel instrumentPanel = my(ChatPanels.class).newPanel(messages.output(), messageSender);
 		jFrame.setLayout(new BorderLayout());
-		//jFrame.add(instrumentPanel, BorderLayout.CENTER);
+		jFrame.add(instrumentPanel, BorderLayout.CENTER);
 		
 		jFrame.setBounds(100, 100, 200, 300);
 	//	jFrame.pack();
