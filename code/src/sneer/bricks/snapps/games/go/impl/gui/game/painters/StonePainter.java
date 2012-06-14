989f2289e01a51702d4a7d10e2d81f026fbdd09d
Lucas <lucass@gmail.com>
2012-06-14 01:47:50 -0300

    Revert "Experimenting with gradients"
    This reverts commit 1b22bc001445d89cebe635c3dcd620f8d7a7edb6.

--- code/src/sneer/bricks/snapps/games/go/impl/gui/game/painters/StonePainter.java
+++ code/src/sneer/bricks/snapps/games/go/impl/gui/game/painters/StonePainter.java
@@ -1,7 +1,5 @@
 package sneer.bricks.snapps.games.go.impl.gui.game.painters;
 
-import java.awt.Color;
-import java.awt.GradientPaint;
 import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 
@@ -22,9 +20,6 @@ public class StonePainter {
 	public void paintStoneOnCoordinates(Graphics2D graphics, float x, float y, boolean dead) {
 		float d = _stoneDiameter;
 		if (dead) d*=0.6;
-		
-		GradientPaint redtowhite = new GradientPaint(0,0,Color.RED,100, 0,Color.WHITE);
-		graphics.setPaint(redtowhite);
 		
 		graphics.fill(new Ellipse2D.Float(x - (d / 2), y - (d / 2), d, d));
 		//wrapping
