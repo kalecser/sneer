cefc69e5b75bed5013123940a1b8b1226c1fac86
Erick Tiago Pintor <erickpintor@gmail.com>
2012-06-15 09:21:07 -0300

    Easier test environments

--- code/src/sneer/bricks/software/folderconfig/testsupport/BrickTestBase.java
+++ code/src/sneer/bricks/software/folderconfig/testsupport/BrickTestBase.java
@@ -3,20 +3,42 @@ package sneer.bricks.software.folderconfig.testsupport;
 import static basis.environments.Environments.my;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.After;
+
+import sneer.bricks.hardware.cpu.threads.tests.BrickTestWithThreads;
+import sneer.bricks.software.folderconfig.FolderConfig;
 import basis.environments.Environment;
 import basis.environments.Environments;
 import basis.lang.Closure;
 
-import sneer.bricks.hardware.cpu.threads.tests.BrickTestWithThreads;
-import sneer.bricks.software.folderconfig.FolderConfig;
-
 
 public abstract class BrickTestBase extends BrickTestWithThreads {
+	
+	private final List<Environment> environments = new ArrayList<Environment>();
 
 	{
 		my(FolderConfig.class).storageFolder().set(new File(tmpFolderName(), "data"));
 		my(FolderConfig.class).tmpFolder()    .set(new File(tmpFolderName(), "tmp" ));
+	}
+	
+	@After
+	public void afterBrickTestBase() {
+		for (Environment env : environments)
+			crash(env);
+		
+		environments.clear();
+	}
+	
+	@Override
+	protected Environment newTestEnvironment(Object... bindings) {
+		Environment ret = super.newTestEnvironment(bindings);
+		configureStorageFolder(ret, "environmentData" + environments.size());
+		configureTmpFolder(ret, "environmentTmp" + environments.size());
+		environments.add(ret);
+		return ret;
 	}
 
 	protected void configureStorageFolder(Environment environment, final String folderName) {
