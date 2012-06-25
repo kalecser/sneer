f8902e235c784e08868a599d726b0173a2f4a755
Lucas <lucass@gmail.com>
2012-06-24 21:21:40 -0300

    Adding FixedCentralizedOffset

--- code/src/sneer/bricks/snapps/games/go/impl/gui/game/GuiPlayer.java
+++ code/src/sneer/bricks/snapps/games/go/impl/gui/game/GuiPlayer.java
@@ -127,7 +127,7 @@ public class GuiPlayer extends JFrame implements BoardListener,Player{
 		Container contentPane = getContentPane();
 		contentPane.setLayout(new BorderLayout());
 		_goBoardPanel = new GoBoardPanel(this,new ToroidalGoBoard(_boardSize),timerFactory, new ToroidalBoardImagePainter(), _side, new MutableOffset());
-//		_goBoardPanel = new GoBoardPanel(this,new GoBoard(_boardSize),timerFactory, new RegularGoBoardImagePainter(), _side);
+//		_goBoardPanel = new GoBoardPanel(this,new GoBoard(_boardSize),timerFactory, new RegularGoBoardImagePainter(), _side, new FixedCentralizedOffset());
 		
 		_goBoardPanel.setBoardListener(this);
 		contentPane.add(_goBoardPanel, BorderLayout.CENTER);
