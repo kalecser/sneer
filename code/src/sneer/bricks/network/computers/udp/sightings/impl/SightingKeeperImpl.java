85c4107df23fed8062b1b1d69c2874576bdcd2a0
Erick Tiago Pintor <erickpintor@gmail.com>
2012-07-30 21:14:20 -0300

    Udp progress: Publish and receive remote sightings

--- code/src/sneer/bricks/network/computers/udp/sightings/impl/SightingKeeperImpl.java
+++ code/src/sneer/bricks/network/computers/udp/sightings/impl/SightingKeeperImpl.java
@@ -5,6 +5,8 @@ import static basis.environments.Environments.my;
 import java.net.InetSocketAddress;
 
 import sneer.bricks.expression.tuples.TupleSpace;
+import sneer.bricks.expression.tuples.remote.RemoteTuples;
+import sneer.bricks.hardware.cpu.lang.contracts.WeakContract;
 import sneer.bricks.identity.seals.Seal;
 import sneer.bricks.identity.seals.contacts.ContactSeals;
 import sneer.bricks.network.computers.udp.sightings.SightingKeeper;
@@ -14,29 +16,46 @@ import sneer.bricks.pulp.reactive.collections.CollectionSignals;
 import sneer.bricks.pulp.reactive.collections.SetRegister;
 import sneer.bricks.pulp.reactive.collections.SetSignal;
 import basis.lang.CacheMap;
+import basis.lang.Consumer;
 import basis.lang.Producer;
 
 class SightingKeeperImpl implements SightingKeeper {
+	
+	static {
+		my(TupleSpace.class).keep(UdpSighting.class);
+	}
+	
 	
 	private final CacheMap<Contact, SetRegister<InetSocketAddress>> addresses = CacheMap.newInstance();
 	private final static Producer<SetRegister<InetSocketAddress>> newSetRegister = new Producer<SetRegister<InetSocketAddress>>() {  @Override public SetRegister<InetSocketAddress> produce() {
 		return my(CollectionSignals.class).newSetRegister();
 	}};
+	
+	
+	@SuppressWarnings("unused")
+	private final WeakContract refToAvoidGC = my(RemoteTuples.class).addSubscription(UdpSighting.class, new Consumer<UdpSighting>() { @Override public void consume(UdpSighting sighting) {
+		Contact contact = my(ContactSeals.class).contactGiven(sighting.peerSeal);
+		if (contact == null) return;
+		keep(contact, new InetSocketAddress(sighting.host, sighting.port));
+	}});
+	
 
 	@Override
 	public void keep(Contact contact, InetSocketAddress sighting) {
 		SetRegister<InetSocketAddress> addrs = getAddresses(contact);
-		if(!addrs.output().currentContains(sighting)) {
+		if (!addrs.output().currentContains(sighting)) {
 			addrs.add(sighting);
 			Seal seal = my(ContactSeals.class).sealGiven(contact).currentValue();
 			my(TupleSpace.class).add(new UdpSighting(seal, sighting.getHostString(), sighting.getPort()));
 		}
 	}
+	
 
 	@Override
 	public SetSignal<InetSocketAddress> sightingsOf(Contact contact) {
 		return getAddresses(contact).output();
 	}
+	
 
 	private SetRegister<InetSocketAddress> getAddresses(Contact contact) {
 		return addresses.get(contact, newSetRegister);
--- code/src/sneer/bricks/network/computers/udp/sightings/tests/SightingKeeperTest.java
+++ code/src/sneer/bricks/network/computers/udp/sightings/tests/SightingKeeperTest.java
@@ -1,44 +1,45 @@
 package sneer.bricks.network.computers.udp.sightings.tests;
 
 import static basis.environments.Environments.my;
+import static basis.environments.Environments.runWith;
 
 import java.net.InetSocketAddress;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
-import sneer.bricks.expression.tuples.TupleSpace;
-import sneer.bricks.hardware.cpu.lang.contracts.WeakContract;
+import sneer.bricks.expression.tuples.testsupport.BrickTestWithTuples;
 import sneer.bricks.identity.seals.Seal;
 import sneer.bricks.identity.seals.contacts.ContactSeals;
 import sneer.bricks.network.computers.udp.sightings.SightingKeeper;
-import sneer.bricks.network.computers.udp.sightings.UdpSighting;
 import sneer.bricks.network.social.Contact;
 import sneer.bricks.network.social.Contacts;
-import sneer.bricks.software.folderconfig.testsupport.BrickTestBase;
+import sneer.bricks.pulp.reactive.collections.SetSignal;
+import basis.lang.Closure;
 import basis.lang.exceptions.Refusal;
-import basis.util.concurrent.RefLatch;
 
-public class SightingKeeperTest extends BrickTestBase {
+public class SightingKeeperTest extends BrickTestWithTuples {
 	
 	private final SightingKeeper subject = my(SightingKeeper.class);
 
-	@Ignore
 	@Test(timeout=2000)
 	public void onSighting_ShouldPublishTuple() throws Refusal {
 		Contact neide = my(Contacts.class).produceContact("Neide");
 		my(ContactSeals.class).put("Neide", new Seal(new byte[] { 42 }));
 		
-		InetSocketAddress sighting = new InetSocketAddress("201.202.203.231", 2021);
-		subject.keep(neide, sighting);
+		runWith(remote(), new Closure() { @Override public void run() {
+			Contact neide = my(Contacts.class).contactGiven("Neide");
+			InetSocketAddress sighting = new InetSocketAddress("201.202.203.231", 2021);
+			subject.keep(neide, sighting);
+		}});
 		
-		RefLatch<UdpSighting> latch = new RefLatch<>();
-		@SuppressWarnings("unused") WeakContract ref = my(TupleSpace.class).addSubscription(UdpSighting.class, latch);
-		UdpSighting tuple = latch.waitAndGet();
+		waitForAllDispatchingToFinish();
 		
-		assertEquals(42, tuple.peerSeal.bytes.get(0));
-		assertEquals("201.202.203.231", tuple.host);
-		assertEquals(2021, tuple.port);
+		SetSignal<InetSocketAddress> sightings = subject.sightingsOf(neide);
+		assertTrue(sightings.size().currentValue() == 1);
+		
+		InetSocketAddress addr = sightings.currentElements().iterator().next();
+		assertEquals("201.202.203.231", addr.getHostString());
+		assertEquals(2021, addr.getPort());
 	}
 
 }
