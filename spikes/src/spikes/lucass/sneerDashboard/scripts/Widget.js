eae956f7c15f908f7073ac7a51d9e68c0cc87cf6
Lucas <lucas@giant.(none)>
2012-08-18 13:19:42 -0300

    Add title bar to widget

--- spikes/src/spikes/lucass/sneerDashboard/scripts/Widget.js
+++ spikes/src/spikes/lucass/sneerDashboard/scripts/Widget.js
@@ -13,8 +13,10 @@ function createColumns(state){
 function appendWidget(widget){
 	$("#widgetColumnWrap"+widget['column']).append(
 		'<div class="widget">'+
-		widget['title']+'<br>'+
-		'<iframe src="'+widget['url']+'" height="'+widget['height']+'""></iframe><br>'+
+			'<div class="widgetTitle">'+widget['title']+'</div>'+
+			'<div class="widgetBody">'+
+				'<iframe src="'+widget['url']+'" height="'+widget['height']+'""></iframe><br>'+
+			'</div>'+
 		'</div>'
 	);
 }
@@ -22,20 +24,21 @@ function appendWidget(widget){
 function appendBlankWidget(column){
 	$("#widgetColumnWrap"+column).append(
 		'<div class="widget">'+
-		'New Widget<br>'+
-		'Title:<br>'+
-		'<input type="text"/><br>'+
-		'Url:<br>'+
-		'<input type="text"/><br>'+
-		'Height:<br>'+
-		'<input type="text"/><br>'+
-		'<input type="button" value="Add"/><br>'+
+			'<div class="widgetTitle">New Widget</div>'+
+			'<div class="widgetBody">'+
+				'Title:<br>'+
+				'<input type="text"/><br>'+
+				'Url:<br>'+
+				'<input type="text"/><br>'+
+				'Height:<br>'+
+				'<input type="text"/><br>'+
+				'<input type="button" value="Add"/><br>'+
+			'</div>'+
 		'</div>'
 	);
 }
 
 function appendColumn(size,index){
-	var c = $('#widgetTable tr:first td').length;
 	$('#widgetTable tr:first').append('<td  style="width:'+size+'"> <a href="javascript:appendBlankWidget('+index+');" >Add widget</a> </td>');
 	$('#widgetTable tr:gt(0)').append('<td class="widgetColumn" style="width:'+size+';"> <div id="widgetColumnWrap'+index+'" class="col"></div> </td>');
 }
