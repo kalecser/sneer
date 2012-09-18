c3eef1509fdfe599ee41158b0ed7ec17fd1a0324
kalecser <kalecser@gmail.com>
2012-09-18 14:41:47 -0300

    minor

--- code/src/sneer/tests/freedom7/Freedom7TestGit.java
+++ code/src/sneer/tests/freedom7/Freedom7TestGit.java
@@ -1,13 +1,12 @@
 package sneer.tests.freedom7;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import sneer.tests.SovereignFunctionalTestBase;
 
 public class Freedom7TestGit extends SovereignFunctionalTestBase {
 
-	@Ignore
+//	@Ignore
 	@Test (timeout = 1000 * 10)
 	public void meToo() throws Exception {
 		//LoggerMocks.showLog = true;
