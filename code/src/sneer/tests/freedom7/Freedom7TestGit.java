96ff0dd381f835da3575b0ec842dc9fa4e69ed42
kalecser <kalecser@gmail.com>
2012-09-18 14:56:58 -0300

    Revert "minor"
    This reverts commit c3eef1509fdfe599ee41158b0ed7ec17fd1a0324.

--- code/src/sneer/tests/freedom7/Freedom7TestGit.java
+++ code/src/sneer/tests/freedom7/Freedom7TestGit.java
@@ -1,12 +1,13 @@
 package sneer.tests.freedom7;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import sneer.tests.SovereignFunctionalTestBase;
 
 public class Freedom7TestGit extends SovereignFunctionalTestBase {
 
-//	@Ignore
+	@Ignore
 	@Test (timeout = 1000 * 10)
 	public void meToo() throws Exception {
 		//LoggerMocks.showLog = true;
