636d84ecb7cea5928ab8be2e76740e7e07a694fd
kalecser <kalecser@gmail.com>
2012-09-22 16:54:24 -0300

    Web sneer commiting for test with two sneers

--- code/src/sneer/bricks/snapps/web/impl/WebImpl.java
+++ code/src/sneer/bricks/snapps/web/impl/WebImpl.java
@@ -3,11 +3,17 @@ package sneer.bricks.snapps.web.impl;
 import static basis.environments.Environments.my;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
 
+import basis.lang.Consumer;
+
+import sneer.bricks.expression.tuples.TupleSpace;
 import sneer.bricks.hardware.cpu.lang.contracts.WeakContract;
+import sneer.bricks.identity.name.OwnName;
 import sneer.bricks.identity.seals.Seal;
 import sneer.bricks.network.computers.http.server.HttpHandler;
 import sneer.bricks.network.computers.http.server.HttpServer;
+import sneer.bricks.snapps.owninfo.OwnInfo;
 import sneer.bricks.snapps.web.Web;
 
 public class WebImpl implements Web {
@@ -15,12 +21,31 @@ public class WebImpl implements Web {
 	WeakContract ref;
 	
 	{
+		
+		
+		my(TupleSpace.class).addSubscription(WebRequest.class, new Consumer<WebRequest>() {@Override public void consume(WebRequest value) {
+				my(TupleSpace.class).add(new WebResponse("I'm here. I am "+my(OwnName.class).toString()));
+		}});
+		
+		final AtomicReference<String> lastContents = new AtomicReference<String>();
+		
+		my(TupleSpace.class).addSubscription(WebResponse.class, new Consumer<WebResponse>() {@Override public void consume(WebResponse value) {
+			lastContents.set(value._contents);
+		}});
+		
 		final SealForUrl sealForUrl = new SealForUrl(new ContactProviderImpl());
 		try {
 			ref = my(HttpServer.class).start(getPort(), new HttpHandler() {@Override public String replyFor(String target) {
 				Seal sealForUrlOrNull = sealForUrl.getSealForUrlOrNull(target);
-				
-				return "<h1>It works!! </h1><br>requestFor: "+sealForUrlOrNull;
+				if(sealForUrlOrNull == null) return "Sorry! <br>Url not found.";
+				Seal sealForUrl = sealForUrlOrNull;
+				my(TupleSpace.class).add(new WebRequest(sealForUrl));
+				String lastContentBroadcasted = lastContents.get();
+				if(!lastContentBroadcasted.isEmpty()){
+					lastContents.set("");
+					return lastContentBroadcasted;
+				}
+				return "<h1>Please refresh!! </h1><br>requestFor: "+sealForUrl;
 			}});
 		} catch (IOException e) {
 			throw new IllegalStateException(e);
