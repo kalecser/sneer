03c83e780879a282a3d2bb53b7a2cd3149088300
Rodrigo B. de Oliveira <bamboo@ubuntu.(none)>
2012-09-22 11:17:22 -0300

    code gardening

--- code/src/sneer/bricks/software/bricks/repl/gui/impl/ReplWindowImpl.java
+++ code/src/sneer/bricks/software/bricks/repl/gui/impl/ReplWindowImpl.java
@@ -28,7 +28,7 @@ public class ReplWindowImpl extends JFrame implements ReplWindow {
 	public ReplWindowImpl() {
 		
 		setLayout(new BorderLayout());
-		add (_title , BorderLayout.NORTH);
+		add(_title , BorderLayout.NORTH);
 	
 		super.add(textArea);
 		super.setSize(300, 300);
@@ -36,7 +36,7 @@ public class ReplWindowImpl extends JFrame implements ReplWindow {
 		textArea.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyReleased(KeyEvent e) {
-				if(e.isControlDown() && e.getKeyCode() == KeyEvent.VK_ENTER) {
+				if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_ENTER) {
 					executeAndShowResult();
 				}
 			}
