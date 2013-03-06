d629764024088ba09b6cadf059a1fe9fdb28a59a
Lucas <lucass@gmail.com>
2013-03-06 10:51:56 -0300

    Fixing endless loop when own seal was on clipboard and when you refused to add a contact

--- code/src/sneer/bricks/network/social/ui/clipboardadder/impl/ClipboardAdderImpl.java
+++ code/src/sneer/bricks/network/social/ui/clipboardadder/impl/ClipboardAdderImpl.java
@@ -3,6 +3,7 @@ package sneer.bricks.network.social.ui.clipboardadder.impl;
 import static basis.environments.Environments.my;
 import sneer.bricks.hardware.clock.timer.Timer;
 import sneer.bricks.hardware.cpu.lang.contracts.WeakContract;
+import sneer.bricks.identity.seals.OwnSeal;
 import sneer.bricks.identity.seals.Seal;
 import sneer.bricks.identity.seals.contacts.ContactSeals;
 import sneer.bricks.network.social.Contact;
@@ -16,17 +17,22 @@ import basis.lang.exceptions.Refusal;
 public class ClipboardAdderImpl implements ClipboardAdder {
 	private Clipboard clip = my(Clipboard.class);
 	private ContactSeals contactSeals = my(ContactSeals.class);
+	private Seal lastSeal;
 	
 	WeakContract timer = my(Timer.class).wakeUpNowAndEvery(3000, new Runnable() { @Override public void run() {
 		Seal seal = null;
 		try {
-			seal = contactSeals.unmarshal(clip.getContent());
+ 			seal = contactSeals.unmarshal(clip.getContent());
 		} catch (Refusal e) {
 			return;
 		}
 		
+		if(seal == null) return;		
 		if(contactSeals.contactGiven(seal) != null) return;
+		if(seal.equals(lastSeal)) return;
+		if(my(OwnSeal.class).get().equals(seal)) return;
 		
+		lastSeal = seal;
 		String nickname = "<New Contact> " + System.currentTimeMillis();
 		Contact contact = my(Contacts.class).produceContact(nickname);
 		try {
