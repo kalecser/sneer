444979e2a859b69e8535f6b0032d9f98c6df2a45
Lucas <lucass@gmail.com>
2013-03-06 14:06:05 -0300

    Continuous deploy script

--- installer/server/continuousDeploy.sh
+++ installer/server/continuousDeploy.sh
@@ -1,5 +1,7 @@
 apt-get -y install openjdk-7-jdk
 cd ~
+
+apt-get -y install junit4
 git clone git://github.com/klauswuestefeld/simploy.git
 cd simploy
-javac Simploy.java 
+javac -cp .:/usr/share/java/junit4.jar Simploy.java
