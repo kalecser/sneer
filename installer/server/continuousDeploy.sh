01e8a81eaf015ae12741df8b1838c2a8c37f012d
Lucas <lucass@gmail.com>
2013-03-19 18:24:28 -0300

    Sneer continuous deploy now adding simploy to machine services

--- installer/server/continuousDeploy.sh
+++ installer/server/continuousDeploy.sh
@@ -1,5 +1,10 @@
 apt-get -y install openjdk-7-jdk
 apt-get -y install ant
+
+cp sneerServerBoot.sh /etc/init.d/
+chmod +x /etc/init.d/sneerServerBoot.sh
+update-rc.d sneerServerBoot.sh defaults 80
+
 cd ~
 
 apt-get -y install junit4
---
+++ installer/server/sneerServerBoot.sh
@@ -0,0 +1,2 @@
+cd /root/sneer/installer
+java -XX:MaxPermSize=550m -cp /root/simploy/:/usr/share/java/junit4.jar Simploy "ant compile" build/tmp/bin/  build/tmp/bin/ "ant deploy" senhamalucaqq
