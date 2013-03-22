afe488fc09834157a9130c4ceeb25dcbbee7a6f7
Lucas <lucass@gmail.com>
2013-03-22 18:53:10 -0300

    Starting all service needed to distribute sneer on agitter.com

--- installer/server/continuousDeploy.sh
+++ installer/server/continuousDeploy.sh
@@ -11,3 +11,11 @@ apt-get -y install junit4
 git clone git://github.com/klauswuestefeld/simploy.git
 cd simploy
 javac -cp .:/usr/share/java/junit4.jar Simploy.java
+
+apt-get install webfs
+sed -i "s#web_root=\"/srv/ftp\"#web_root=\"/root/sneer/installer/build\"#" /etc/webfsd.conf
+sed -i "s#web_port=\"\"#web_port=\"80\"#" /etc/webfsd.conf
+sed -i "s#web_user=\"www-data\"#web_user=\"root\"#" /etc/webfsd.conf
+sed -i "s#web_group=\"www-data\"#web_group=\"root\"#" /etc/webfsd.conf
+
+service sneerServerBoot.sh start
--- installer/server/sneerServerBoot.sh
+++ installer/server/sneerServerBoot.sh
@@ -1,4 +1,8 @@
-cd /root/sneer/installer/build
-python -m SimpleHTTPServer 80 &
 cd /root/sneer/installer
-java -XX:MaxPermSize=550m -cp /root/simploy/:/usr/share/java/junit4.jar Simploy "ant compile" build/tmp/bin/  build/tmp/bin/ "ant deploy" senhamalucaqq
+java -XX:MaxPermSize=550m -cp /root/simploy/:/usr/share/java/junit4.jar Simploy "ant compile" build/tmp/bin/  build/tmp/bin/ "ant deploy" senhamalucaqq&
+
+ant compile
+mkdir -p /root/stun/code
+cp -rf build/tmp/bin /root/stun/code
+cd /root/stun/code/bin
+java -cp . sneer.main.stun.StunServerSession&
