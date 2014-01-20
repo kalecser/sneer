6683a0327c848c4fd0dbcff4f67c6f0a79aab355
Rodrigo B. de Oliveira <rbo@acm.org>
2014-01-20 19:16:20 -0200

    simulate whole process including adding a new file and pushing at the end

--- bazaar/project.clj
+++ bazaar/project.clj
@@ -1,7 +1,5 @@
 (defproject bazaar "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.1"] [clj-jgit "0.6.4"] [org.clojure/clojure-contrib "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-jgit "0.6.4"]])
 
-
-;; (def my-repo
-;;  (git-clone-full "https://github.com/semperos/clj-jgit.git" "local-folder/clj-jgit")
--- bazaar/src/bazaar/core.clj
+++ bazaar/src/bazaar/core.clj
@@ -1,49 +1,41 @@
-;(ns bazaar.core
-;  (:use [clj-jgit.porcelain] [clojure.contrib.java.utils]))
-;  (:use [clojure.contrib.java-utils]))
-
 (ns bazaar.core
-  (:require clojure.contrib.java-utils))
+  (:use clj-jgit.porcelain))
 
 (def repo-name "trash")
 
 (def repo-url (str "git@github.com:klauswuestefeld/" repo-name ".git"))
 
-(delete-file-recursively repo-name)
+(fs.core/delete-dir "trash")
 
 (with-identity {repo-name {}}
-  (def my-clone
-    (git-clone-full repo-url repo-name)))
+    (def my-clone (git-clone-full repo-url repo-name))
+    (def pull-result (.call (.pull (:repo my-clone)))))
 
-(def pull-result (.call (.pull (:repo my-clone))))
 (.isSuccessful pull-result)
 
 (def my-repo (:repo my-clone))
 
 (git-status my-repo)
-;=> {:untracked #{}, :removed #{}, :modified #{}, :missing #{}, :changed #{}, :added #{}}
+
 
 ;; List existing branches
 (git-branch-list my-repo)
-;=> (#<LooseUnpeeled Ref[refs/heads/master=526f58f0b09621ce27fbae575991c8311a515430]>)
 
 ;; Create a new local branch to store our changes
 (git-branch-create my-repo "my-branch")
-;=> #<LooseUnpeeled Ref[refs/heads/my-branch=526f58f0b09621ce27fbae575991c8311a515430]>
 
 ;; Prove to ourselves that it was created
 (git-branch-list my-repo)
-;=> (#<LooseUnpeeled Ref[refs/heads/master=526f58f0b09621ce27fbae575991c8311a515430]> #<LooseUnpeeled Ref[refs/heads/my-branch=526f58f0b09621ce27fbae575991c8311a515430]>)
 
 ;; Check out our new branch
-
 (git-checkout my-repo "my-branch")
-;=> #<LooseUnpeeled Ref[refs/heads/my-branch=526f58f0b09621ce27fbae575991c8311a515430]>
 
 ;; Now go off and make your changes.
+(with-open [f (clojure.java.io/writer (str repo-name java.io.File/separator "whatever.txt"))]
+  (.write f "test"))
+
 ;; For example, let's say we added a file "foo.txt" at the base of the project.
 (git-status my-repo)
-;=> {:untracked #{"foo.txt"}, :removed #{}, :modified #{}, :missing #{}, :changed #{}, :added #{}}
 
 ;; Add the file to the index
 (git-add my-repo "whatever.txt")
@@ -51,20 +43,19 @@
 
 ;; Check for status change
 (git-status my-repo)
-;=> {:untracked #{}, :removed #{}, :modified #{}, :missing #{}, :changed #{}, :added #{"foo.txt"}}
 
 ;; Now commit your changes, specifying author and committer if desired
 (git-commit my-repo "Add a file" {:name "Fulano" :email "fulano@example.com"})
-;=> #<RevCommit commit 5e116173db370bf400b3514a4b093ec3d98a2666 1310135270 -----p>
-
 
 ;; Status clean
 (git-status my-repo)
-;=> {:untracked #{}, :removed #{}, :modified #{}, :missing #{}, :changed #{}, :added #{}}
 
 (git-clean my-repo :clean-dirs? true, :ignore? true)
 
 (first (git-log my-repo))
 
-(with-identity {"trash" {}}
-  (-> my-repo .push .call))
+(def final-push
+  (with-identity {"trash" {}}
+    (-> my-repo .push .call)))
+
+(-> final-push first .getRemoteUpdates first)
