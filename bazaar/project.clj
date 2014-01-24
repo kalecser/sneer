c2ad19347dfbea1cac2cce34d04949ceeddccd33
Rodrigo B. de Oliveira <rbo@acm.org>
2014-01-24 15:53:03 -0200

    import tentacles

--- bazaar/project.clj
+++ bazaar/project.clj
@@ -3,5 +3,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-jgit "0.6.4"]
                  [http-kit "2.1.16"]
-                 [enlive "1.1.5"]]
+                 [enlive "1.1.5"]
+                 [tentacles "0.2.5"]]
   :main bazaar.main)
